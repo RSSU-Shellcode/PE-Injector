6a2813de850aeb5e37712d53fb5f4263f54eec97
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-07-04 13:42:49 +0800

    add TestInjector for develop and test, check image and shellcode.

--- injector_test.go
+++ injector_test.go
@@ -1,9 +1,37 @@
 package injector
 
 import (
+	"os"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestInjector(t *testing.T) {
+	injector := NewInjector()
 
+	t.Run("x86", func(t *testing.T) {
+		image, err := os.ReadFile("testdata/image_x86.dat")
+		require.NoError(t, err)
+		shellcode, err := os.ReadFile("testdata/shellcode_x86.dat")
+		require.NoError(t, err)
+
+		output, err := injector.Inject(shellcode, image, nil)
+		require.NoError(t, err)
+		require.NotEmpty(t, output)
+	})
+
+	t.Run("x64", func(t *testing.T) {
+		image, err := os.ReadFile("testdata/image_x64.dat")
+		require.NoError(t, err)
+		shellcode, err := os.ReadFile("testdata/shellcode_x64.dat")
+		require.NoError(t, err)
+
+		output, err := injector.Inject(shellcode, image, nil)
+		require.NoError(t, err)
+		require.NotEmpty(t, output)
+	})
+
+	err := injector.Close()
+	require.NoError(t, err)
 }
