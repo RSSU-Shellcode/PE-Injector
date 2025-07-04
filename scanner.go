2072a98fdee5ab6426b3d402af1d73669eb4c2bc
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-07-04 12:53:15 +0800

    finish code cave scanner, maybe need adjust constants.

---
+++ scanner.go
@@ -0,0 +1,99 @@
+package injector
+
+import (
+	"debug/pe"
+	"errors"
+)
+
+// 0x00, 0x00, [n * 0x00], 0x00, 0x00
+// 0xCC, 0xCC, [n * 0xCC], 0xCC, 0xCC
+
+const (
+	jmpInstSize    = 5
+	reserveSize    = 2
+	minCaveSizeX86 = 2*reserveSize + (6 + jmpInstSize)
+	minCaveSizeX64 = 2*reserveSize + (12 + jmpInstSize)
+	minNumCaves    = 64
+)
+
+type codeCave struct {
+	addr int
+	size int
+}
+
+func (inj *Injector) scanCodeCave() error {
+	var text *pe.Section
+	for _, section := range inj.img.Sections {
+		if section.Name == ".text" {
+			text = section
+			break
+		}
+	}
+	if text == nil {
+		return errors.New("cannot find .text section in image")
+	}
+	// record offset and calculate scan range
+	offset := text.Offset
+	size := text.Size
+	if text.VirtualSize < size {
+		size = text.VirtualSize
+	}
+	if size < 32*1024 {
+		return errors.New(".text section too small")
+	}
+	section, err := text.Data()
+	if err != nil {
+		return err
+	}
+	section = section[:size-32]
+	// scan code caves
+	caves := inj.scanSection(section, int(offset))
+	if len(caves) < minNumCaves {
+		return errors.New("too little code caves")
+	}
+	inj.caves = caves
+	return nil
+}
+
+func (inj *Injector) scanSection(section []byte, offset int) []*codeCave {
+	var caves []*codeCave
+	for addr := 0; addr < len(section); addr++ {
+		caveSize := 1
+		switch section[addr] {
+		case 0x00:
+			for j := addr + 1; j < len(section); j++ {
+				if section[j] != 0x00 {
+					break
+				}
+				caveSize++
+			}
+		case 0xCC:
+			for j := addr + 1; j < len(section); j++ {
+				if section[j] != 0xCC {
+					break
+				}
+				caveSize++
+			}
+		default:
+			continue
+		}
+		// check the cave size is enough
+		var minCaveSize int
+		switch inj.arch {
+		case "386":
+			minCaveSize = minCaveSizeX86
+		case "amd64":
+			minCaveSize = minCaveSizeX64
+		}
+		if caveSize < minCaveSize {
+			addr += caveSize
+			continue
+		}
+		caves = append(caves, &codeCave{
+			addr: offset + addr + reserveSize,
+			size: caveSize - 2*reserveSize,
+		})
+		addr += caveSize
+	}
+	return caves
+}
