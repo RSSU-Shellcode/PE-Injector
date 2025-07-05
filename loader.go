015ffe33e432421ad48750b6ea7619613bde5613
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-07-05 14:51:49 +0800

    move code about shellcode loader builder to here, add toRegDWORD.

--- loader.go
+++ loader.go
@@ -1,10 +1,13 @@
 package injector
 
 import (
+	"bytes"
 	"embed"
 	"encoding/hex"
 	"fmt"
+	"strconv"
 	"strings"
+	"text/template"
 )
 
 // just for prevent [import _ "embed"] :)
@@ -22,8 +25,22 @@ var (
 )
 
 var (
-	separator = string([]byte{0x8F, 0x17, 0x97, 0x3C})
-	gpaOffset = []byte{0x33, 0x22, 0x11, 0xFF}
+	registerX86 = []string{
+		"eax", "ebx", "ecx",
+		"edx", "esi", "edi",
+	}
+
+	registerX64 = []string{
+		"rax", "rbx", "rcx",
+		"rdx", "rsi", "rdi",
+		"r8", "r9", "r10", "r11",
+		"r12", "r13", "r14", "r15",
+	}
+)
+
+var (
+	separator = []byte{0x8F, 0x17, 0x97, 0x3C} // split each instruction
+	gpaOffset = []byte{0x33, 0x22, 0x11, 0xFF} // GetProcAddress offset in IAT
 )
 
 type loaderCtx struct {
@@ -31,7 +48,96 @@ type loaderCtx struct {
 	Reg map[string]string
 
 	// for split each instruction
-	Separator string
+	Separator []byte
+}
+
+func (inj *Injector) buildLoader() ([][]byte, error) {
+	var (
+		src string
+	)
+	switch inj.arch {
+	case "386":
+		src = inj.getLoaderX86()
+	case "amd64":
+		src = inj.getLoaderX64()
+	}
+	// create assembly source
+	tpl, err := template.New("loader").Funcs(template.FuncMap{
+		"db":  toDB,
+		"hex": toHex,
+		"dr":  toRegDWORD,
+	}).Parse(src)
+	if err != nil {
+		return nil, fmt.Errorf("invalid assembly source template: %s", err)
+	}
+	ctx := loaderCtx{
+		Reg:       inj.buildRandomRegisterMap(),
+		Separator: separator,
+	}
+	buf := bytes.NewBuffer(make([]byte, 0, 4096))
+	err = tpl.Execute(buf, &ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to build assembly source: %s", err)
+	}
+	inst, err := inj.assemble(buf.String())
+	if err != nil {
+		return nil, err
+	}
+	fmt.Println(buf.String())
+	fmt.Println(len(inst))
+	return nil, nil
+}
+
+func (inj *Injector) getLoaderX86() string {
+	if inj.opts.LoaderX86 != "" {
+		return inj.opts.LoaderX86
+	}
+	return defaultLoaderX86
+}
+
+func (inj *Injector) getLoaderX64() string {
+	if inj.opts.LoaderX64 != "" {
+		return inj.opts.LoaderX64
+	}
+	return defaultLoaderX64
+}
+
+func (inj *Injector) buildRandomRegisterMap() map[string]string {
+	inj.initRegisterBox()
+	register := make(map[string]string, 16)
+	switch inj.arch {
+	case "386":
+		for _, reg := range registerX86 {
+			register[reg] = inj.selectRegister()
+		}
+	case "amd64":
+		for _, reg := range registerX64 {
+			register[reg] = inj.selectRegister()
+		}
+	}
+	return register
+}
+
+func (inj *Injector) initRegisterBox() {
+	var reg []string
+	switch inj.arch {
+	case "386":
+		reg = make([]string, len(registerX86))
+		copy(reg, registerX86)
+	case "amd64":
+		reg = make([]string, len(registerX64))
+		copy(reg, registerX64)
+	}
+	inj.regBox = reg
+}
+
+// selectRegister is used to make sure each register will be selected once.
+func (inj *Injector) selectRegister() string {
+	idx := inj.rand.Intn(len(inj.regBox))
+	reg := inj.regBox[idx]
+	// remove selected register
+	inj.regBox = append(inj.regBox[:idx], inj.regBox[idx+1:]...)
+	return reg
 }
 
 func toDB(b []byte) string {
@@ -51,4 +157,13 @@ func toDB(b []byte) string {
 
 func toHex(v any) string {
 	return fmt.Sprintf("0x%X", v)
+}
+
+// convert r8 -> r8d, rax -> eax
+func toRegDWORD(reg string) string {
+	_, err := strconv.Atoi(reg[1:])
+	if err == nil {
+		return reg + "d"
+	}
+	return strings.ReplaceAll(reg, "r", "e")
 }
