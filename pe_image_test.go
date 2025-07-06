5cb45b2db5eb151bfa31c853ed5d546016b8ae74
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-07-06 20:03:35 +0800

    add TestLoadImage for check IAT from the test Go image files.

---
+++ pe_image_test.go
@@ -0,0 +1,48 @@
+package injector
+
+import (
+	"bytes"
+	"debug/pe"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestLoadImage(t *testing.T) {
+	injector := NewInjector()
+
+	t.Run("x86", func(t *testing.T) {
+		image, err := os.ReadFile("testdata/image_x86.dat")
+		require.NoError(t, err)
+		peFile, err := pe.NewFile(bytes.NewReader(image))
+		require.NoError(t, err)
+		injector.img = peFile
+		injector.arch = "386"
+
+		injector.loadImage(image)
+
+		for _, iat := range injector.iat {
+			fmt.Println(iat.dll, iat.proc, iat.addr)
+		}
+	})
+
+	t.Run("x64", func(t *testing.T) {
+		image, err := os.ReadFile("testdata/image_x64.dat")
+		require.NoError(t, err)
+		peFile, err := pe.NewFile(bytes.NewReader(image))
+		require.NoError(t, err)
+		injector.img = peFile
+		injector.arch = "amd64"
+
+		injector.loadImage(image)
+
+		for _, iat := range injector.iat {
+			fmt.Println(iat.dll, iat.proc, iat.addr)
+		}
+	})
+
+	err := injector.Close()
+	require.NoError(t, err)
+}
