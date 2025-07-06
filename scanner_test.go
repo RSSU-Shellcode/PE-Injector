a735f0f89dc17cc3ff057b1269e935762dfefe4d
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-07-06 14:55:35 +0800

    add TestScanCodeCave for adjust constant maxInstSizeX86.

---
+++ scanner_test.go
@@ -0,0 +1,52 @@
+package injector
+
+import (
+	"bytes"
+	"debug/pe"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestScanCodeCave(t *testing.T) {
+	injector := NewInjector()
+
+	t.Run("x86", func(t *testing.T) {
+		image, err := os.ReadFile("testdata/image_x86.dat")
+		require.NoError(t, err)
+		peFile, err := pe.NewFile(bytes.NewReader(image))
+		require.NoError(t, err)
+		injector.img = peFile
+		injector.arch = "386"
+
+		err = injector.scanCodeCave()
+		require.NoError(t, err)
+
+		fmt.Println(len(injector.caves))
+		for _, cave := range injector.caves {
+			fmt.Println(cave.addr, cave.size)
+		}
+	})
+
+	t.Run("x64", func(t *testing.T) {
+		image, err := os.ReadFile("testdata/image_x64.dat")
+		require.NoError(t, err)
+		peFile, err := pe.NewFile(bytes.NewReader(image))
+		require.NoError(t, err)
+		injector.img = peFile
+		injector.arch = "amd64"
+
+		err = injector.scanCodeCave()
+		require.NoError(t, err)
+
+		fmt.Println(len(injector.caves))
+		for _, cave := range injector.caves {
+			fmt.Println(cave.addr, cave.size)
+		}
+	})
+
+	err := injector.Close()
+	require.NoError(t, err)
+}
