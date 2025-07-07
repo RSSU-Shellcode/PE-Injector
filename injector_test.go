73a889a1fdf4448f7a5129d5ef071e7133d8b96f
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-07-07 13:14:37 +0800

    add TestSpecificSeed for debug output image files.

--- injector_test.go
+++ injector_test.go
@@ -35,3 +35,43 @@ func TestInjector(t *testing.T) {
 	err := injector.Close()
 	require.NoError(t, err)
 }
+
+// TODO add more tests
+func TestSpecificSeed(t *testing.T) {
+	injector := NewInjector()
+
+	opts := &Options{
+		RandSeed: 1234,
+	}
+
+	t.Run("x86", func(t *testing.T) {
+		image, err := os.ReadFile("testdata/image_x86.dat")
+		require.NoError(t, err)
+		shellcode, err := os.ReadFile("testdata/shellcode_x86.dat")
+		require.NoError(t, err)
+
+		output, err := injector.Inject(shellcode, image, opts)
+		require.NoError(t, err)
+		require.NotEmpty(t, output)
+
+		err = os.WriteFile("testdata/injected_x86.exe", output, 0600)
+		require.NoError(t, err)
+	})
+
+	t.Run("x64", func(t *testing.T) {
+		image, err := os.ReadFile("testdata/image_x64.dat")
+		require.NoError(t, err)
+		shellcode, err := os.ReadFile("testdata/shellcode_x64.dat")
+		require.NoError(t, err)
+
+		output, err := injector.Inject(shellcode, image, opts)
+		require.NoError(t, err)
+		require.NotEmpty(t, output)
+
+		err = os.WriteFile("testdata/injected_x64.exe", output, 0600)
+		require.NoError(t, err)
+	})
+
+	err := injector.Close()
+	require.NoError(t, err)
+}
