bbe57ecb5c65ee408a23040947e564d7ede04db3
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-07-11 13:02:45 +0800

    improve TestInjector for support various options, update TestSpecificSeed.

--- injector_test.go
+++ injector_test.go
@@ -10,30 +10,92 @@ import (
 func TestInjector(t *testing.T) {
 	injector := NewInjector()
 
-	t.Run("x86", func(t *testing.T) {
-		image, err := os.ReadFile("testdata/image_x86.dat")
-		require.NoError(t, err)
-		shellcode, err := os.ReadFile("testdata/shellcode_x86.dat")
-		require.NoError(t, err)
-
-		output, err := injector.Inject(image, shellcode, nil)
-		require.NoError(t, err)
-		require.NotEmpty(t, output)
+	t.Run("common", func(t *testing.T) {
+		testInjector(t, injector, nil)
 	})
 
-	t.Run("x64", func(t *testing.T) {
-		image, err := os.ReadFile("testdata/image_x64.dat")
-		require.NoError(t, err)
-		shellcode, err := os.ReadFile("testdata/shellcode_x64.dat")
-		require.NoError(t, err)
+	t.Run("set address", func(t *testing.T) {
+		opts := Options{
+			Address: 0x469D20,
+		}
+		testInjector(t, injector, &opts)
+	})
 
-		output, err := injector.Inject(image, shellcode, nil)
-		require.NoError(t, err)
-		require.NotEmpty(t, output)
+	t.Run("not save context", func(t *testing.T) {
+		opts := Options{
+			NotSaveContext: true,
+		}
+		testInjector(t, injector, &opts)
 	})
 
 	err := injector.Close()
 	require.NoError(t, err)
+}
+
+func testInjector(t *testing.T, injector *Injector, opts *Options) {
+	t.Run("loader", func(t *testing.T) {
+		t.Run("x86", func(t *testing.T) {
+			image, err := os.ReadFile("testdata/image_x86.dat")
+			require.NoError(t, err)
+			shellcode, err := os.ReadFile("testdata/shellcode_x86.dat")
+			require.NoError(t, err)
+
+			output, err := injector.Inject(image, shellcode, opts)
+			require.NoError(t, err)
+			require.NotEmpty(t, output)
+
+			err = os.WriteFile("testdata/injected_x86.exe", output, 0600)
+			require.NoError(t, err)
+		})
+
+		t.Run("x64", func(t *testing.T) {
+			image, err := os.ReadFile("testdata/image_x64.dat")
+			require.NoError(t, err)
+			shellcode, err := os.ReadFile("testdata/shellcode_x64.dat")
+			require.NoError(t, err)
+
+			output, err := injector.Inject(image, shellcode, opts)
+			require.NoError(t, err)
+			require.NotEmpty(t, output)
+
+			err = os.WriteFile("testdata/injected_x64.exe", output, 0600)
+			require.NoError(t, err)
+		})
+	})
+
+	t.Run("raw", func(t *testing.T) {
+		t.Run("x86", func(t *testing.T) {
+			image, err := os.ReadFile("testdata/image_x86.dat")
+			require.NoError(t, err)
+			shellcode := []byte{
+				0x90,
+				0x66, 0x90,
+			}
+
+			output, err := injector.InjectRaw(image, shellcode, opts)
+			require.NoError(t, err)
+			require.NotEmpty(t, output)
+
+			err = os.WriteFile("testdata/injected_x86.exe", output, 0600)
+			require.NoError(t, err)
+		})
+
+		t.Run("x64", func(t *testing.T) {
+			image, err := os.ReadFile("testdata/image_x64.dat")
+			require.NoError(t, err)
+			shellcode := []byte{
+				0x90,
+				0x66, 0x90,
+			}
+
+			output, err := injector.InjectRaw(image, shellcode, opts)
+			require.NoError(t, err)
+			require.NotEmpty(t, output)
+
+			err = os.WriteFile("testdata/injected_x64.exe", output, 0600)
+			require.NoError(t, err)
+		})
+	})
 }
 
 // TODO add more tests
@@ -41,6 +103,7 @@ func TestSpecificSeed(t *testing.T) {
 	injector := NewInjector()
 
 	opts := &Options{
+		Address:  0x469D20,
 		RandSeed: 1234,
 	}
 
@@ -74,16 +137,16 @@ func TestSpecificSeed(t *testing.T) {
 		})
 	})
 
-	t.Run("segment", func(t *testing.T) {
+	t.Run("raw", func(t *testing.T) {
 		t.Run("x86", func(t *testing.T) {
 			image, err := os.ReadFile("testdata/image_x86.dat")
 			require.NoError(t, err)
-			shellcode := [][]byte{
-				{0x90},
-				{0x66, 0x90},
+			shellcode := []byte{
+				0x90,
+				0x66, 0x90,
 			}
 
-			output, err := injector.InjectSegment(image, shellcode, opts)
+			output, err := injector.InjectRaw(image, shellcode, opts)
 			require.NoError(t, err)
 			require.NotEmpty(t, output)
 
@@ -94,12 +157,12 @@ func TestSpecificSeed(t *testing.T) {
 		t.Run("x64", func(t *testing.T) {
 			image, err := os.ReadFile("testdata/image_x64.dat")
 			require.NoError(t, err)
-			shellcode := [][]byte{
-				{0x90},
-				{0x66, 0x90},
+			shellcode := []byte{
+				0x90,
+				0x66, 0x90,
 			}
 
-			output, err := injector.InjectSegment(image, shellcode, opts)
+			output, err := injector.InjectRaw(image, shellcode, opts)
 			require.NoError(t, err)
 			require.NotEmpty(t, output)
 
