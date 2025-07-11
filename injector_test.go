4f8577d88d85f3679ce852282aa2cb8f01db9170
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-07-11 13:42:38 +0800

    add TestSpecificAddress, remove sub test about set Address.

--- injector_test.go
+++ injector_test.go
@@ -14,13 +14,6 @@ func TestInjector(t *testing.T) {
 		testInjector(t, injector, nil)
 	})
 
-	t.Run("set address", func(t *testing.T) {
-		opts := Options{
-			Address: 0x469D20,
-		}
-		testInjector(t, injector, &opts)
-	})
-
 	t.Run("not save context", func(t *testing.T) {
 		opts := Options{
 			NotSaveContext: true,
@@ -96,6 +89,93 @@ func testInjector(t *testing.T, injector *Injector, opts *Options) {
 			require.NoError(t, err)
 		})
 	})
+}
+
+func TestSpecificAddress(t *testing.T) {
+	injector := NewInjector()
+
+	t.Run("loader", func(t *testing.T) {
+		t.Run("x86", func(t *testing.T) {
+			opts := &Options{
+				Address: 0x46A590,
+			}
+
+			image, err := os.ReadFile("testdata/image_x86.dat")
+			require.NoError(t, err)
+			shellcode, err := os.ReadFile("testdata/shellcode_x86.dat")
+			require.NoError(t, err)
+
+			output, err := injector.Inject(image, shellcode, opts)
+			require.NoError(t, err)
+			require.NotEmpty(t, output)
+
+			err = os.WriteFile("testdata/injected_x86.exe", output, 0600)
+			require.NoError(t, err)
+		})
+
+		t.Run("x64", func(t *testing.T) {
+			opts := &Options{
+				Address: 0x469D20,
+			}
+
+			image, err := os.ReadFile("testdata/image_x64.dat")
+			require.NoError(t, err)
+			shellcode, err := os.ReadFile("testdata/shellcode_x64.dat")
+			require.NoError(t, err)
+
+			output, err := injector.Inject(image, shellcode, opts)
+			require.NoError(t, err)
+			require.NotEmpty(t, output)
+
+			err = os.WriteFile("testdata/injected_x64.exe", output, 0600)
+			require.NoError(t, err)
+		})
+	})
+
+	t.Run("raw", func(t *testing.T) {
+		t.Run("x86", func(t *testing.T) {
+			opts := &Options{
+				Address: 0x46A590,
+			}
+
+			image, err := os.ReadFile("testdata/image_x86.dat")
+			require.NoError(t, err)
+			shellcode := []byte{
+				0x90,
+				0x66, 0x90,
+			}
+
+			output, err := injector.InjectRaw(image, shellcode, opts)
+			require.NoError(t, err)
+			require.NotEmpty(t, output)
+
+			err = os.WriteFile("testdata/injected_x86.exe", output, 0600)
+			require.NoError(t, err)
+		})
+
+		t.Run("x64", func(t *testing.T) {
+			opts := &Options{
+				Address: 0x469D20,
+			}
+
+			image, err := os.ReadFile("testdata/image_x64.dat")
+			require.NoError(t, err)
+			shellcode := []byte{
+				0x90,
+				0x66, 0x90,
+			}
+
+			output, err := injector.InjectRaw(image, shellcode, opts)
+			require.NoError(t, err)
+			require.NotEmpty(t, output)
+
+			err = os.WriteFile("testdata/injected_x64.exe", output, 0600)
+			require.NoError(t, err)
+		})
+	})
+
+	err := injector.Close()
+	require.NoError(t, err)
 }
 
 // TODO add more tests
