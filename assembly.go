341645f55947f2121089ddbff65bc557c9676599
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-07-12 18:28:25 +0800

    add decodeInst for decode single instruction, update disassemble.

--- assembly.go
+++ assembly.go
@@ -2,36 +2,14 @@ package injector
 
 import (
 	"encoding/binary"
+	"fmt"
 
 	"golang.org/x/arch/x86/x86asm"
 )
 
 const nearJumpSize = 1 + 4
 
-func (inj *Injector) disassemble(src []byte) ([]*x86asm.Inst, error) {
-	var mode int
-	switch inj.arch {
-	case "386":
-		mode = 32
-	case "amd64":
-		mode = 64
-	}
-	var insts []*x86asm.Inst
-	for len(src) > 0 {
-		inst, err := x86asm.Decode(src, mode)
-		if err != nil {
-			return nil, err
-		}
-		insts = append(insts, &inst)
-		src = src[inst.Len:]
-	}
-	return insts, nil
-}
-
-// relocateInstruction is used to relocate instruction like jmp, call...
-// [Warning]: it is only partially done.
-// #nosec G115
-func (inj *Injector) relocateInstruction(src []byte, offset int64) []byte {
+func (inj *Injector) decodeInst(src []byte) (*x86asm.Inst, error) {
 	var mode int
 	switch inj.arch {
 	case "386":
@@ -41,10 +19,50 @@ func (inj *Injector) relocateInstruction(src []byte, offset int64) []byte {
 	}
 	inst, err := x86asm.Decode(src, mode)
 	if err != nil {
+		return nil, err
+	}
+	return &inst, nil
+}
+
+func (inj *Injector) disassemble(src []byte) ([]*x86asm.Inst, error) {
+	var insts []*x86asm.Inst
+	for len(src) > 0 {
+		inst, err := inj.decodeInst(src)
+		if err != nil {
+			return nil, err
+		}
+		insts = append(insts, inst)
+		src = src[inst.Len:]
+	}
+	return insts, nil
+}
+
+// relocateInstruction is used to relocate instruction like jmp, call...
+// [Warning]: it is only partially done.
+// #nosec G115
+func (inj *Injector) relocateInstruction(src []byte, offset int64) []byte {
+	inst, err := inj.decodeInst(src)
+	if err != nil {
 		panic(err)
 	}
+	fmt.Println(inst.AddrSize, inst.DataSize, inst.MemBytes, len(src), inst.PCRel, inst.PCRelOff)
+
 	output := make([]byte, len(src))
 	copy(output, src)
+	switch inst.PCRel {
+	case 0:
+	case 1:
+		rel := int64(inst.Args[0].(x86asm.Rel))
+		output[1] = uint8(rel - offset)
+	case 2:
+		rel := int64(inst.Args[0].(x86asm.Rel))
+		binary.LittleEndian.PutUint16(output[1:], uint16(rel-offset))
+	case 4:
+		rel := int64(inst.Args[0].(x86asm.Rel))
+		binary.LittleEndian.PutUint32(output[1:], uint32(rel-offset))
+	}
+	return output
+
 	switch inst.Op {
 	case x86asm.CALL:
 		switch output[0] {
@@ -60,6 +78,9 @@ func (inj *Injector) relocateInstruction(src []byte, offset int64) []byte {
 		}
 	case x86asm.JMP:
 		switch output[0] {
+		case 0xEB:
+			rel := int64(inst.Args[0].(x86asm.Rel))
+			output[1] = uint8(rel - offset)
 		case 0xE9:
 			rel := int64(inst.Args[0].(x86asm.Rel))
 			binary.LittleEndian.PutUint32(output[1:], uint32(rel-offset))
@@ -73,6 +94,13 @@ func (inj *Injector) relocateInstruction(src []byte, offset int64) []byte {
 				}
 			}
 		}
+	case x86asm.JG:
+	case x86asm.JGE:
+	case x86asm.JL:
+	case x86asm.JLE:
+	case x86asm.JE:
+	case x86asm.JNE:
+
 	default:
 	}
 	return output
