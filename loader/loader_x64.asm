65851a730c7d00d9a67dabb5a80fe136ca8bf511
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-07-14 15:28:18 +0800

    loader_x64.asm: improve code style for find procedure address.

--- loader/loader_x64.asm
+++ loader/loader_x64.asm
@@ -1,41 +1,42 @@
 .code64
 
 // r10 store address of ImageBaseAddress
-// r11 store address of CreateThread
-// r12 store address of VirtualAlloc
-// r13 store address of VirtualProtect
+// r11 store address of kernel32.dll
+// r12 store address of GetProcAddress
+// r13 store address of CreateThread
+// r14 store address of VirtualAlloc
+// r15 store address of VirtualProtect
 
 entry:
-
-// find CreateThread, VirtualAlloc and VirtualProtect
+  // find CreateThread, VirtualAlloc and VirtualProtect
 {{if .LackProcedure}}
   // push kernel32.dll\0 to stack
-  mov {{.Reg.rax}}, {{index .Kernel32DLLDB 0}}    {{is}}
-  mov {{.Reg.r8}},  {{index .Kernel32DLLKey 0}}   {{is}}
-  xor {{.Reg.rax}}, {{.Reg.r8}}                   {{is}}
-  push {{.Reg.rax}}                               {{is}}
-  mov {{.Reg.rbx}}, {{index .Kernel32DLLDB 1}}    {{is}}
-  mov {{.Reg.r9}},  {{index .Kernel32DLLKey 1}}   {{is}}
-  xor {{.Reg.rbx}}, {{.Reg.r9}}                   {{is}}
-  push {{.Reg.rbx}}                               {{is}}
+  mov {{.Reg.rax}}, {{index .Kernel32DLLDB 0}}
+  mov {{.Reg.r8}},  {{index .Kernel32DLLKey 0}}
+  xor {{.Reg.rax}}, {{.Reg.r8}}
+  push {{.Reg.rax}}
+  mov {{.Reg.rbx}}, {{index .Kernel32DLLDB 1}}
+  mov {{.Reg.r9}},  {{index .Kernel32DLLKey 1}}
+  xor {{.Reg.rbx}}, {{.Reg.r9}}
+  push {{.Reg.rbx}}
 
   {{if .LoadLibraryWOnly}}
-  mov {{.Reg.rcx}}, {{index .Kernel32DLLDB 2}}    {{is}}
-  mov {{.Reg.r10}}, {{index .Kernel32DLLKey 2}}   {{is}}
-  xor {{.Reg.rcx}}, {{.Reg.r10}}                  {{is}}
-  push {{.Reg.rcx}}                               {{is}}
-  mov {{.Reg.rdx}}, {{index .Kernel32DLLDB 3}}    {{is}}
-  mov {{.Reg.r11}}, {{index .Kernel32DLLKey 3}}   {{is}}
-  xor {{.Reg.rdx}}, {{.Reg.r11}}                  {{is}}
-  push {{.Reg.rdx}}                               {{is}}
+    mov {{.Reg.rcx}}, {{index .Kernel32DLLDB 2}}
+    mov {{.Reg.r10}}, {{index .Kernel32DLLKey 2}}
+    xor {{.Reg.rcx}}, {{.Reg.r10}}
+    push {{.Reg.rcx}}
+    mov {{.Reg.rdx}}, {{index .Kernel32DLLDB 3}}
+    mov {{.Reg.r11}}, {{index .Kernel32DLLKey 3}}
+    xor {{.Reg.rdx}}, {{.Reg.r11}}
+    push {{.Reg.rdx}}
   {{end}}
 
   // get a pointer to the PEB
   xor {{.Reg.rax}}, {{.Reg.rax}}
   mov {{.Reg.rax}}, 0x60
   mov {{.Reg.rbx}}, gs:[{{.Reg.rax}}]
-  // read image base address
-  mov {{.RegS.r10}}, [{{.Reg.rbx}}+0x10]
+  // store image base address
+  mov {{.RegS.r10}}, [{{.Reg.rbx}} + 0x10]
 
   // read the LoadLibraryA/W form IAT
   mov {{.RegS.rbx}}, {{.RegS.r10}}
@@ -48,43 +49,54 @@ entry:
   call {{.RegS.rbx}}
   add rsp, 0x20
 
-  add rsp, 4*8
-  int3
+  // store the handle of kernel32.dll
+  mov {{.RegS.r11}}, rax
 
-  // store DLL handle in stable register
-  mov {{.RegS.rbx}}, rax
-
-  // restore stack
+  // restore stack for "kernel32.dll"
   {{if .LoadLibraryWOnly}}
-  add rsp, 4*8
+    add rsp, 4*8
   {{else}}
-  add rsp, 2*8
+    add rsp, 2*8
   {{end}}
 
   // read the GetProcAddress form IAT
-  mov {{.Reg.rax}}, [rip + 0xFF112201]
+  mov {{.Reg.rax}}, {{.RegS.r10}}
+  add {{.Reg.rax}}, {{hex .GetProcAddress}}
+  mov {{.RegS.r12}}, [{{.Reg.rax}}]
 
+  // get the procedure address
   {{if .LackCreateThread}}
 
   {{else}}
-  mov {{.RegS.r10}}, [rip + 0xFF112210]
+    mov {{.Reg.rbx}}, {{.RegS.r10}}
+    add {{.Reg.rbx}}, {{hex .CreateThread}}
+    mov {{.RegS.r13}}, [{{.Reg.rbx}}]
   {{end}}
 
   {{if .LackVirtualAlloc}}
 
   {{else}}
-  mov {{.RegS.r11}}, [rip + 0xFF112211]
+    mov {{.Reg.rcx}}, {{.RegS.r10}}
+    add {{.Reg.rcx}}, {{hex .VirtualAlloc}}
+    mov {{.RegS.r14}}, [{{.Reg.rcx}}]
   {{end}}
 
   {{if .LackVirtualProtect}}
 
   {{else}}
-  mov {{.RegS.r12}}, [rip + 0xFF112212]
+    mov {{.Reg.rdx}}, {{.RegS.r10}}
+    add {{.Reg.rdx}}, {{hex .VirtualProtect}}
+    mov {{.RegS.r15}}, [{{.Reg.rdx}}]
   {{end}}
 
 {{else}}
-  mov {{.RegS.r10}}, [rip + 0xFF112210]
-  mov {{.RegS.r11}}, [rip + 0xFF112211]
-  mov {{.RegS.r12}}, [rip + 0xFF112212]
+  // get a pointer to the PEB
+  xor {{.Reg.rax}}, {{.Reg.rax}}
+  mov {{.Reg.rax}}, 0x60
+  mov {{.Reg.rbx}}, gs:[{{.Reg.rax}}]
+  // store image base address
+  mov {{.RegS.r10}}, [{{.Reg.rbx}} + 0x10]
+
 {{end}}
 
+  int3
