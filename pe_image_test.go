63b5edea8bf5aae9b3636d1e02b1d20ea76f87e9
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-07-17 13:47:27 +0800

    add sub tests about extend section for write large data.

--- pe_image_test.go
+++ pe_image_test.go
@@ -104,6 +104,58 @@ func TestExtendSection(t *testing.T) {
 		})
 	})
 
+	t.Run("extend", func(t *testing.T) {
+		t.Run("x86", func(t *testing.T) {
+			image, err := os.ReadFile("testdata/image_x86.dat")
+			require.NoError(t, err)
+			peFile, err := pe.NewFile(bytes.NewReader(image))
+			require.NoError(t, err)
+			injector.img = peFile
+			injector.arch = "386"
+			err = injector.preprocess(image, nil)
+			require.NoError(t, err)
+
+			data := bytes.Repeat([]byte("Hello Injector!"), 1024)
+			rva := injector.extendSection(data)
+			fmt.Printf("rva: 0x%X\n", rva)
+
+			output := injector.dup
+
+			peFile, err = pe.NewFile(bytes.NewReader(output))
+			require.NoError(t, err)
+			last := peFile.Sections[len(peFile.Sections)-1]
+			require.Greater(t, last.VirtualSize, uint32(len(data)))
+			require.Greater(t, last.Size, uint32(len(data)))
+
+			testExecuteImage(t, "testdata/injected_x86.exe", output)
+		})
+
+		t.Run("x64", func(t *testing.T) {
+			image, err := os.ReadFile("testdata/image_x64.dat")
+			require.NoError(t, err)
+			peFile, err := pe.NewFile(bytes.NewReader(image))
+			require.NoError(t, err)
+			injector.img = peFile
+			injector.arch = "amd64"
+			err = injector.preprocess(image, nil)
+			require.NoError(t, err)
+
+			data := bytes.Repeat([]byte("Hello Injector!"), 1024)
+			rva := injector.extendSection(data)
+			fmt.Printf("rva: 0x%X\n", rva)
+
+			output := injector.dup
+
+			peFile, err = pe.NewFile(bytes.NewReader(output))
+			require.NoError(t, err)
+			last := peFile.Sections[len(peFile.Sections)-1]
+			require.Greater(t, last.VirtualSize, uint32(len(data)))
+			require.Greater(t, last.Size, uint32(len(data)))
+
+			testExecuteImage(t, "testdata/injected_x64.exe", output)
+		})
+	})
+
 	err := injector.Close()
 	require.NoError(t, err)
 }
