c5d2d44dbad4e4258e27d5cd456d0133a75770dd
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-07-19 13:05:40 +0800

    fix bug in extendInstruction about new instruction offset.

--- assembly.go
+++ assembly.go
@@ -49,8 +49,15 @@ func (inj *Injector) extendInstruction(inst *x86asm.Inst, src []byte) []byte {
 		jmp := make([]byte, 5)
 		jmp[0] = 0xE9
 		rel := uint32(inst.Args[0].(x86asm.Rel))
-		binary.LittleEndian.PutUint32(jmp[inst.PCRelOff:], rel)
+		binary.LittleEndian.PutUint32(jmp[1:], rel)
 		return jmp
+	case x86asm.JE:
+		je := make([]byte, 6)
+		je[0] = 0x0F
+		je[1] = 0x84
+		rel := uint32(inst.Args[0].(x86asm.Rel))
+		binary.LittleEndian.PutUint32(je[2:], rel)
+		return je
 	default:
 		return src
 	}
@@ -79,46 +86,46 @@ func (inj *Injector) relocateInstruction(src []byte, offset int64) []byte {
 		binary.LittleEndian.PutUint32(output[inst.PCRelOff:], uint32(rel-offset))
 	}
 	return output
-
-	switch inst.Op {
-	case x86asm.CALL:
-		switch output[0] {
-		case 0xFF:
-			switch output[1] {
-			case 0x15:
-				mem := inst.Args[0].(x86asm.Mem)
-				binary.LittleEndian.PutUint32(output[2:], uint32(mem.Disp-offset))
-			}
-		case 0xE8:
-			mem := inst.Args[0].(x86asm.Mem)
-			binary.LittleEndian.PutUint32(output[1:], uint32(mem.Disp-offset))
-		}
-	case x86asm.JMP:
-		switch output[0] {
-		case 0xEB:
-			rel := int64(inst.Args[0].(x86asm.Rel))
-			output[1] = uint8(rel - offset)
-		case 0xE9:
-			rel := int64(inst.Args[0].(x86asm.Rel))
-			binary.LittleEndian.PutUint32(output[1:], uint32(rel-offset))
-		case 0x48:
-			switch output[1] {
-			case 0xFF:
-				switch output[2] {
-				case 0x25:
-					mem := inst.Args[0].(x86asm.Mem)
-					binary.LittleEndian.PutUint32(output[3:], uint32(mem.Disp-offset))
-				}
-			}
-		}
-	case x86asm.JG:
-	case x86asm.JGE:
-	case x86asm.JL:
-	case x86asm.JLE:
-	case x86asm.JE:
-	case x86asm.JNE:
-
-	default:
-	}
-	return output
 }
+
+// switch inst.Op {
+// case x86asm.CALL:
+// 	switch output[0] {
+// 	case 0xFF:
+// 		switch output[1] {
+// 		case 0x15:
+// 			mem := inst.Args[0].(x86asm.Mem)
+// 			binary.LittleEndian.PutUint32(output[2:], uint32(mem.Disp-offset))
+// 		}
+// 	case 0xE8:
+// 		mem := inst.Args[0].(x86asm.Mem)
+// 		binary.LittleEndian.PutUint32(output[1:], uint32(mem.Disp-offset))
+// 	}
+// case x86asm.JMP:
+// 	switch output[0] {
+// 	case 0xEB:
+// 		rel := int64(inst.Args[0].(x86asm.Rel))
+// 		output[1] = uint8(rel - offset)
+// 	case 0xE9:
+// 		rel := int64(inst.Args[0].(x86asm.Rel))
+// 		binary.LittleEndian.PutUint32(output[1:], uint32(rel-offset))
+// 	case 0x48:
+// 		switch output[1] {
+// 		case 0xFF:
+// 			switch output[2] {
+// 			case 0x25:
+// 				mem := inst.Args[0].(x86asm.Mem)
+// 				binary.LittleEndian.PutUint32(output[3:], uint32(mem.Disp-offset))
+// 			}
+// 		}
+// 	}
+// case x86asm.JG:
+// case x86asm.JGE:
+// case x86asm.JL:
+// case x86asm.JLE:
+// case x86asm.JE:
+// case x86asm.JNE:
+//
+// default:
+// }
+// return output
