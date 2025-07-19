523911f9cbbffa3440b4466508762186e4de9b8e
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-07-19 13:04:17 +0800

    relocate_x64.asm: add more statement about je and jmp.

--- testdata/relocate_x64.asm
+++ testdata/relocate_x64.asm
@@ -3,18 +3,31 @@
 entry:
   mov rax, 0x1123
   mov rcx, 0x2313
-
   jmp next
+
 fn2:
   mov r8, 0x3545
   ret
 
-next:
-  xor rax, rax
+je2:
   call fn2
-  call fn1
   nop
   int3
+
+next:
+  xor rax, rax
+  test rax, rax
+  je je1
+  nop
+  nop
+  nop
+je1:
+  call fn1
+  call fn2
+  xor rax, rax
+  test rax, rax
+  je je2
+  nop
 
 fn1:
   mov r9, 0x456F
