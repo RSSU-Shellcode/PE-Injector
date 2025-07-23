7a4e950a8053708d20237789a80cb854ee840286
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-07-23 14:23:24 +0800

    add TestAnalyze for analyze the pe images in testdata directory.

--- analyze_test.go
+++ analyze_test.go
@@ -1,1 +1,31 @@
 package injector
+
+import (
+	"os"
+	"testing"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAnalyze(t *testing.T) {
+	t.Run("x86", func(t *testing.T) {
+		image, err := os.ReadFile("testdata/image_x86.dat")
+		require.NoError(t, err)
+
+		info, err := Analyze(image)
+		require.NoError(t, err)
+
+		spew.Dump(info)
+	})
+
+	t.Run("x64", func(t *testing.T) {
+		image, err := os.ReadFile("testdata/image_x64.dat")
+		require.NoError(t, err)
+
+		info, err := Analyze(image)
+		require.NoError(t, err)
+
+		spew.Dump(info)
+	})
+}
