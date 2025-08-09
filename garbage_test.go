b7cc469740ae337515495a081ad4d2138211aa4f
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-08-09 12:52:43 +0800

    add TestGarbage and TestGarbageTemplateFuzz for test junk code.

--- garbage_test.go
+++ garbage_test.go
@@ -1,1 +1,77 @@
 package injector
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestGarbage(t *testing.T) {
+	injector := NewInjector()
+
+	opts := &Options{
+		ForceCreateSection: true,
+	}
+
+	t.Run("x86", func(t *testing.T) {
+		image, err := os.ReadFile("testdata/image_x86.dat")
+		require.NoError(t, err)
+		shellcode, err := os.ReadFile("testdata/shellcode_x86.dat")
+		require.NoError(t, err)
+
+		ctx, err := injector.Inject(image, shellcode, opts)
+		require.NoError(t, err)
+
+		testExecuteImage(t, "testdata/injected_x86.exe", ctx.Output)
+	})
+
+	t.Run("x64", func(t *testing.T) {
+		image, err := os.ReadFile("testdata/image_x64.dat")
+		require.NoError(t, err)
+		shellcode, err := os.ReadFile("testdata/shellcode_x64.dat")
+		require.NoError(t, err)
+
+		ctx, err := injector.Inject(image, shellcode, opts)
+		require.NoError(t, err)
+
+		testExecuteImage(t, "testdata/injected_x64.exe", ctx.Output)
+	})
+
+	err := injector.Close()
+	require.NoError(t, err)
+}
+
+func TestGarbageTemplateFuzz(t *testing.T) {
+	t.Run("x86", func(t *testing.T) {
+		injector := NewInjector()
+		injector.arch = "386"
+		injector.opts = new(Options)
+		err := injector.initAssembler()
+		require.NoError(t, err)
+
+		for i := 0; i < 1000; i++ {
+			data := injector.garbageTemplate()
+			require.NotEmpty(t, data)
+		}
+
+		err = injector.Close()
+		require.NoError(t, err)
+	})
+
+	t.Run("x64", func(t *testing.T) {
+		injector := NewInjector()
+		injector.arch = "amd64"
+		injector.opts = new(Options)
+		err := injector.initAssembler()
+		require.NoError(t, err)
+
+		for i := 0; i < 1000; i++ {
+			data := injector.garbageTemplate()
+			require.NotEmpty(t, data)
+		}
+
+		err = injector.Close()
+		require.NoError(t, err)
+	})
+}
