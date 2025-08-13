a128dace3f380a7005a4ec3097f481cbda9ef8a9
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-08-13 14:51:32 +0800

    update TestInspectLoaderTemplate and TestInspectJunkCodeTemplate.

--- inspect_test.go
+++ inspect_test.go
@@ -19,13 +19,19 @@ func TestInspectLoaderTemplate(t *testing.T) {
 		for i1 := 0; i1 < 2; i1++ {
 			opt.HasVirtualAlloc = i1 == 0
 			for i2 := 0; i2 < 2; i2++ {
-				opt.HasVirtualProtect = i2 == 0
+				opt.HasVirtualFree = i2 == 0
 				for i3 := 0; i3 < 2; i3++ {
-					opt.HasCreateThread = i3 == 0
+					opt.HasVirtualProtect = i3 == 0
 					for i4 := 0; i4 < 2; i4++ {
-						opt.HasLoadLibraryA = i4 == 0
-						cp := opt
-						opts = append(opts, &cp)
+						opt.HasCreateThread = i4 == 0
+						for i5 := 0; i5 < 2; i5++ {
+							opt.HasWaitForSingleObject = i5 == 0
+							for i6 := 0; i6 < 2; i6++ {
+								opt.HasLoadLibraryA = i6 == 0
+								cp := opt
+								opts = append(opts, &cp)
+							}
+						}
 					}
 				}
 			}
@@ -35,8 +41,11 @@ func TestInspectLoaderTemplate(t *testing.T) {
 	t.Run("x86", func(t *testing.T) {
 		for _, opt := range opts {
 			asm, inst, err := InspectLoaderTemplate("386", defaultLoaderX86, opt)
-			require.NoError(t, err)
-			fmt.Println(asm)
+			require.NoError(t, err, asm)
+			insts, err := disassemble(inst, 32)
+			require.NoError(t, err, inst)
+
+			fmt.Println("num of instructions:", len(insts))
 			spew.Dump(inst)
 		}
 	})
@@ -44,8 +53,11 @@ func TestInspectLoaderTemplate(t *testing.T) {
 	t.Run("x64", func(t *testing.T) {
 		for _, opt := range opts {
 			asm, inst, err := InspectLoaderTemplate("amd64", defaultLoaderX64, opt)
-			require.NoError(t, err)
-			fmt.Println(asm)
+			require.NoError(t, err, asm)
+			insts, err := disassemble(inst, 64)
+			require.NoError(t, err, inst)
+
+			fmt.Println("num of instructions:", len(insts))
 			spew.Dump(inst)
 		}
 	})
@@ -55,8 +67,11 @@ func TestInspectJunkCodeTemplate(t *testing.T) {
 	t.Run("x86", func(t *testing.T) {
 		for _, src := range defaultJunkCodeX86 {
 			asm, inst, err := InspectJunkCodeTemplate("386", src)
-			require.NoError(t, err)
-			fmt.Println(asm)
+			require.NoError(t, err, asm)
+			insts, err := disassemble(inst, 32)
+			require.NoError(t, err, inst)
+
+			fmt.Println("num of instructions:", len(insts))
 			spew.Dump(inst)
 		}
 	})
@@ -64,8 +79,11 @@ func TestInspectJunkCodeTemplate(t *testing.T) {
 	t.Run("x64", func(t *testing.T) {
 		for _, src := range defaultJunkCodeX64 {
 			asm, inst, err := InspectJunkCodeTemplate("amd64", src)
-			require.NoError(t, err)
-			fmt.Println(asm)
+			require.NoError(t, err, asm)
+			insts, err := disassemble(inst, 64)
+			require.NoError(t, err, inst)
+
+			fmt.Println("num of instructions:", len(insts))
 			spew.Dump(inst)
 		}
 	})
