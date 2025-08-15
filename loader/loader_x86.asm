a00849193b7c8628ad6b96de8ce84fc4c329fef8
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-08-15 14:04:44 +0800

    loader_x86.asm: replace the register from x64 to x86, update stdcall.

--- loader/loader_x86.asm
+++ loader/loader_x86.asm
@@ -28,31 +28,43 @@ entry:
 {{if .LackProcedure}}
   // push kernel32 module name to stack
   mov {{.Reg.eax}}, {{index .Kernel32DLLDB  0}}                {{igi}}
-  mov {{.Reg.r8}},  {{index .Kernel32DLLKey 0}}                {{igi}}
-  xor {{.Reg.eax}}, {{.Reg.r8}}                                {{igi}}
+  mov {{.Reg.ecx}}, {{index .Kernel32DLLKey 0}}                {{igi}}
+  xor {{.Reg.eax}}, {{.Reg.ecx}}                               {{igi}}
   push {{.Reg.eax}}                                            {{igi}}
   mov {{.Reg.ebx}}, {{index .Kernel32DLLDB  1}}                {{igi}}
-  mov {{.Reg.r9}},  {{index .Kernel32DLLKey 1}}                {{igi}}
-  xor {{.Reg.ebx}}, {{.Reg.r9}}                                {{igi}}
+  mov {{.Reg.edx}}, {{index .Kernel32DLLKey 1}}                {{igi}}
+  xor {{.Reg.ebx}}, {{.Reg.edx}}                               {{igi}}
   push {{.Reg.ebx}}                                            {{igi}}
+  mov {{.Reg.edx}}, {{index .Kernel32DLLDB  2}}                {{igi}}
+  mov {{.Reg.ebp}}, {{index .Kernel32DLLKey 2}}                {{igi}}
+  xor {{.Reg.edx}}, {{.Reg.ebp}}                               {{igi}}
+  push {{.Reg.edx}}                                            {{igi}}
+  mov {{.Reg.edi}}, {{index .Kernel32DLLDB  3}}                {{igi}}
+  mov {{.Reg.esi}}, {{index .Kernel32DLLKey 3}}                {{igi}}
+  xor {{.Reg.edi}}, {{.Reg.esi}}                               {{igi}}
+  push {{.Reg.edi}}                                            {{igi}}
 
   {{if .LoadLibraryWOnly}}
-    mov {{.Reg.ecx}}, {{index .Kernel32DLLDB  2}}              {{igi}}
-    mov {{.Reg.r10}}, {{index .Kernel32DLLKey 2}}              {{igi}}
-    xor {{.Reg.ecx}}, {{.Reg.r10}}                             {{igi}}
-    push {{.Reg.ecx}}                                          {{igi}}
-    mov {{.Reg.rdx}}, {{index .Kernel32DLLDB  3}}              {{igi}}
-    mov {{.Reg.r11}}, {{index .Kernel32DLLKey 3}}              {{igi}}
-    xor {{.Reg.rdx}}, {{.Reg.r11}}                             {{igi}}
-    push {{.Reg.rdx}}                                          {{igi}}
+    mov {{.Reg.ebx}}, {{index .Kernel32DLLDB  4}}              {{igi}}
+    mov {{.Reg.edx}}, {{index .Kernel32DLLKey 4}}              {{igi}}
+    xor {{.Reg.ebx}}, {{.Reg.edx}}                             {{igi}}
+    push {{.Reg.ebx}}                                          {{igi}}
+    mov {{.Reg.edi}}, {{index .Kernel32DLLDB  5}}              {{igi}}
+    mov {{.Reg.esi}}, {{index .Kernel32DLLKey 5}}              {{igi}}
+    xor {{.Reg.edi}}, {{.Reg.esi}}                             {{igi}}
+    push {{.Reg.edi}}                                          {{igi}}
+    mov {{.Reg.eax}}, {{index .Kernel32DLLDB  6}}              {{igi}}
+    mov {{.Reg.ecx}}, {{index .Kernel32DLLKey 6}}              {{igi}}
+    xor {{.Reg.eax}}, {{.Reg.ecx}}                             {{igi}}
+    push {{.Reg.eax}}                                          {{igi}}
   {{end}}
 
   // get pointer to the PEB
   xor {{.Reg.eax}}, {{.Reg.eax}}                               {{igi}}
-  mov {{.Reg.eax}}, 0x60                                       {{igi}}
-  mov {{.Reg.ebx}}, gs:[{{.Reg.eax}}]                          {{igi}}
+  mov {{.Reg.eax}}, 0x30                                       {{igi}}
+  mov {{.Reg.ebx}}, fs:[{{.Reg.eax}}]                          {{igi}}
   // store image base address
-  mov {{.RegN.edi}}, [{{.Reg.ebx}} + 0x10]                     {{igi}}
+  mov {{.RegN.edi}}, [{{.Reg.ebx}} + 0x08]                     {{igi}}
 
   // read the LoadLibraryA/W form IAT
   mov {{.RegN.ebx}}, {{.RegN.edi}}                             {{igi}}
@@ -60,58 +72,59 @@ entry:
   mov {{.RegN.ebx}}, [{{.RegN.ebx}}]                           {{igi}}
 
   // load kernel32.dll
-  mov ecx, esp         {{igi}} // lpLibFileName
-  sub esp, 0x20        {{igi}} // reserve stack for call convention
+  push esp             {{igi}} // lpLibFileName
   call {{.RegN.ebx}}   {{igi}} // call LoadLibraryA/W
-  add esp, 0x20        {{igi}} // restore stack for call convention
 
   // store the handle of kernel32.dll
   mov {{.RegN.esi}}, eax                                       {{igi}}
 
   // restore stack for kernel32 module name
   {{if .LoadLibraryWOnly}}
-    add esp, 8*4                                               {{igi}}
+    add esp, 7*4                                               {{igi}}
   {{else}}
     add esp, 4*4                                               {{igi}}
   {{end}}
 
+
+
+
 {{else}}
   // get pointer to the PEB
   xor {{.Reg.eax}}, {{.Reg.eax}}                               {{igi}}
-  mov {{.Reg.eax}}, 0x60                                       {{igi}}
-  mov {{.Reg.ebx}}, gs:[{{.Reg.eax}}]                          {{igi}}
+  mov {{.Reg.eax}}, 0x30                                       {{igi}}
+  mov {{.Reg.ebx}}, fs:[{{.Reg.eax}}]                          {{igi}}
   // store image base address
-  mov {{.RegN.edi}}, [{{.Reg.ebx}} + 0x10]                     {{igi}}
+  mov {{.RegN.edi}}, [{{.Reg.ebx}} + 0x08]                     {{igi}}
   // get procedure address of VirtualAlloc
-  mov {{.RegV.ecx}}, {{.RegN.edi}}                             {{igi}}
-  add {{.RegV.ecx}}, {{hex .VirtualAlloc}}                     {{igi}}
-  mov {{.RegV.ecx}}, [{{.RegV.ecx}}]                           {{igi}}
-  mov [esp+0x10], {{.RegV.ecx}}                                {{igi}}
+  mov {{.RegV.eax}}, {{.RegN.edi}}                             {{igi}}
+  add {{.RegV.eax}}, {{hex .VirtualAlloc}}                     {{igi}}
+  mov {{.RegV.eax}}, [{{.RegV.eax}}]                           {{igi}}
+  mov [esp+0x08], {{.RegV.eax}}                                {{igi}}
   // get procedure address of VirtualFree
   {{if .NeedEraseShellcode}}
     mov {{.RegV.ecx}}, {{.RegN.edi}}                           {{igi}}
     add {{.RegV.ecx}}, {{hex .VirtualFree}}                    {{igi}}
     mov {{.RegV.ecx}}, [{{.RegV.ecx}}]                         {{igi}}
-    mov [esp+0x18], {{.RegV.ecx}}                              {{igi}}
+    mov [esp+0x0C], {{.RegV.ecx}}                              {{igi}}
   {{end}}
   // get procedure address of VirtualProtect
-  mov {{.RegV.rdx}}, {{.RegN.edi}}                             {{igi}}
-  add {{.RegV.rdx}}, {{hex .VirtualProtect}}                   {{igi}}
-  mov {{.RegV.rdx}}, [{{.RegV.rdx}}]                           {{igi}}
-  mov [esp+0x20], {{.RegV.rdx}}                                {{igi}}
+  mov {{.RegV.edx}}, {{.RegN.edi}}                             {{igi}}
+  add {{.RegV.edx}}, {{hex .VirtualProtect}}                   {{igi}}
+  mov {{.RegV.edx}}, [{{.RegV.edx}}]                           {{igi}}
+  mov [esp+0x10], {{.RegV.edx}}                                {{igi}}
   // get procedure address of CreateThread
   {{if .NeedCreateThread}}
-    mov {{.RegV.r8}}, {{.RegN.edi}}                            {{igi}}
-    add {{.RegV.r8}}, {{hex .CreateThread}}                    {{igi}}
-    mov {{.RegV.r8}}, [{{.RegV.r8}}]                           {{igi}}
-    mov [esp+0x28], {{.RegV.r8}}                               {{igi}}
+    mov {{.RegV.eax}}, {{.RegN.edi}}                           {{igi}}
+    add {{.RegV.eax}}, {{hex .CreateThread}}                   {{igi}}
+    mov {{.RegV.eax}}, [{{.RegV.eax}}]                         {{igi}}
+    mov [esp+0x14], {{.RegV.eax}}                              {{igi}}
   {{end}}
-  // get procedure address of WaitFoesingleObject
+  // get procedure address of WaitForSingleObject
   {{if .NeedWaitThread}}
-    mov {{.RegV.r9}}, {{.RegN.edi}}                            {{igi}}
-    add {{.RegV.r9}}, {{hex .WaitFoesingleObject}}             {{igi}}
-    mov {{.RegV.r9}}, [{{.RegV.r9}}]                           {{igi}}
-    mov [esp+0x30], {{.RegV.r9}}                               {{igi}}
+    mov {{.RegV.ecx}}, {{.RegN.edi}}                           {{igi}}
+    add {{.RegV.ecx}}, {{hex .WaitForSingleObject}}            {{igi}}
+    mov {{.RegV.ecx}}, [{{.RegV.ecx}}]                         {{igi}}
+    mov [esp+0x18], {{.RegV.ecx}}                              {{igi}}
   {{end}}
 {{end}} // LackProcedure
 
