2dfc7aa94a1d670cf9424027d8f18fac35870c63
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-08-25 13:01:13 +0800

    rename InspectOptions to InspectConfig, update InspectLoaderTemplate.

--- inspect.go
+++ inspect.go
@@ -5,8 +5,8 @@ import (
 	"strings"
 )
 
-// InspectOptions contains options about inspect loader template.
-type InspectOptions struct {
+// InspectConfig contains configuration about inspect loader template.
+type InspectConfig struct {
 	CodeCaveMode      bool `toml:"code_cave_mode"      json:"code_cave_mode"`
 	ExtendSectionMode bool `toml:"extend_section_mode" json:"extend_section_mode"`
 	CreateSectionMode bool `toml:"create_section_mode" json:"create_section_mode"`
@@ -20,10 +20,9 @@ type InspectOptions struct {
 }
 
 // InspectLoaderTemplate is used to test junk code template.
-func InspectLoaderTemplate(arch string, src string, opts *InspectOptions) (string, []byte, error) {
+func InspectLoaderTemplate(arch string, src string, cfg *InspectConfig) (string, []byte, error) {
 	switch arch {
-	case "386":
-	case "amd64":
+	case "386", "amd64":
 	default:
 		return "", nil, fmt.Errorf("unsupported architecture: %s", arch)
 	}
@@ -32,16 +31,16 @@ func InspectLoaderTemplate(arch string, src string, opts *InspectOptions) (strin
 	injector.opts = &Options{
 		NoGarbage: true,
 
-		ForceCodeCave:      opts.CodeCaveMode,
-		ForceExtendSection: opts.ExtendSectionMode,
-		ForceCreateSection: opts.CreateSectionMode,
+		ForceCodeCave:      cfg.CodeCaveMode,
+		ForceExtendSection: cfg.ExtendSectionMode,
+		ForceCreateSection: cfg.CreateSectionMode,
 	}
 	injector.ctx = new(Context)
 	injector.dup = make([]byte, 16*1024)
 	injector.caves = []*codeCave{
 		{0x10000, 0x1000, 32},
 	}
-	injector.iat = buildFakeIATList(opts)
+	injector.iat = buildFakeIATList(cfg)
 	src = strings.ReplaceAll(src, codeCaveModeStub, "")
 	err := injector.initAssembler()
 	if err != nil {
@@ -62,44 +61,44 @@ func InspectLoaderTemplate(arch string, src string, opts *InspectOptions) (strin
 	return asm, inst, nil
 }
 
-func buildFakeIATList(opts *InspectOptions) []*iat {
+func buildFakeIATList(cfg *InspectConfig) []*iat {
 	var list []*iat
-	if opts.HasVirtualAlloc {
+	if cfg.HasVirtualAlloc {
 		list = append(list, &iat{
 			dll:  "kernel32.dll",
 			proc: "VirtualAlloc",
 			addr: 0x2000,
 		})
 	}
-	if opts.HasVirtualFree {
+	if cfg.HasVirtualFree {
 		list = append(list, &iat{
 			dll:  "kernel32.dll",
 			proc: "VirtualFree",
 			addr: 0x3000,
 		})
 	}
-	if opts.HasVirtualProtect {
+	if cfg.HasVirtualProtect {
 		list = append(list, &iat{
 			dll:  "kernel32.dll",
 			proc: "VirtualProtect",
 			addr: 0x4000,
 		})
 	}
-	if opts.HasCreateThread {
+	if cfg.HasCreateThread {
 		list = append(list, &iat{
 			dll:  "kernel32.dll",
 			proc: "CreateThread",
 			addr: 0x5000,
 		})
 	}
-	if opts.HasWaitForSingleObject {
+	if cfg.HasWaitForSingleObject {
 		list = append(list, &iat{
 			dll:  "kernel32.dll",
 			proc: "WaitForSingleObject",
 			addr: 0x6000,
 		})
 	}
-	if opts.HasLoadLibraryA {
+	if cfg.HasLoadLibraryA {
 		list = append(list, &iat{
 			dll:  "kernel32.dll",
 			proc: "LoadLibraryA",
@@ -123,8 +122,7 @@ func buildFakeIATList(opts *InspectOptions) []*iat {
 // InspectJunkCodeTemplate is used to test junk code template.
 func InspectJunkCodeTemplate(arch string, src string) (string, []byte, error) {
 	switch arch {
-	case "386":
-	case "amd64":
+	case "386", "amd64":
 	default:
 		return "", nil, fmt.Errorf("unsupported architecture: %s", arch)
 	}
