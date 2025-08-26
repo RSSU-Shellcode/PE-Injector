52499bd4f568d51c3f39311965f453e4c5f6a4ff
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-08-26 14:49:29 +0800

    implement TestTemplate, add template argument, add sub tests.

--- template_test.go
+++ template_test.go
@@ -1,13 +1,86 @@
 package injector
 
 import (
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestTemplate(t *testing.T) {
+	loaderX86 := `
+.code32
+`
+	loaderX64 := `
+.code64
+`
+	tpl := &Template{
+		LoaderX86:     loaderX86,
+		LoaderX64:     loaderX64,
+		MaxNumInstX86: 150,
+		MaxNumInstX64: 100,
 
+		Integer: map[string]uint32{
+			"Const_1": 123,
+			"Const_2": 456,
+		},
+
+		ANSI: map[string]string{
+			"ANSI_1": "ansi",
+		},
+
+		UTF16: map[string]string{
+			"UTF16_1": "utf16",
+		},
+
+		Arguments: map[string]any{
+			"Arg_1": 123,
+			"Arg_2": "string1",
+		},
+
+		Switches: map[string]bool{
+			"Switch_1": true,
+			"Switch_2": false,
+		},
+	}
+
+	injector := NewInjector()
+
+	opts := &Options{
+		Template: tpl,
+	}
+
+	t.Run("x86", func(t *testing.T) {
+		image, err := os.ReadFile("testdata/image_x86.dat")
+		require.NoError(t, err)
+		shellcode, err := os.ReadFile("testdata/shellcode_x86.dat")
+		require.NoError(t, err)
+
+		ctx, err := injector.Inject(image, shellcode, opts)
+		require.NoError(t, err)
+		fmt.Println(ctx.Loader[0])
+		fmt.Println(ctx.Loader[1])
+
+		testExecuteImage(t, "testdata/injected_x86.exe", ctx.Output)
+	})
+
+	t.Run("x64", func(t *testing.T) {
+		image, err := os.ReadFile("testdata/image_x64.dat")
+		require.NoError(t, err)
+		shellcode, err := os.ReadFile("testdata/shellcode_x64.dat")
+		require.NoError(t, err)
+
+		ctx, err := injector.Inject(image, shellcode, opts)
+		require.NoError(t, err)
+		fmt.Println(ctx.Loader[0])
+		fmt.Println(ctx.Loader[1])
+
+		testExecuteImage(t, "testdata/injected_x64.exe", ctx.Output)
+	})
+
+	err := injector.Close()
+	require.NoError(t, err)
 }
 
 func TestTemplate_Check(t *testing.T) {
