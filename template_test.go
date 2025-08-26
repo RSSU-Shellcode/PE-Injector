b2770da10d15dd29bb187f35c24b4c5e5cbdd385
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-08-26 14:26:38 +0800

    add TestTemplate_Check, finish sub tests about empty loader.

--- template_test.go
+++ template_test.go
@@ -2,8 +2,71 @@ package injector
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func TestTemplate(t *testing.T) {
 
+}
+
+func TestTemplate_Check(t *testing.T) {
+	t.Run("common", func(t *testing.T) {
+		tpl := Template{
+			LoaderX86:     ".code32",
+			LoaderX64:     ".code64",
+			MaxNumInstX86: 86,
+			MaxNumInstX64: 64,
+		}
+
+		err := tpl.Check()
+		require.NoError(t, err)
+	})
+
+	t.Run("empty loader", func(t *testing.T) {
+		t.Run("x86", func(t *testing.T) {
+			tpl := Template{}
+
+			err := tpl.Check()
+			errStr := "empty loader template for x86"
+			require.EqualError(t, err, errStr)
+		})
+
+		t.Run("x64", func(t *testing.T) {
+			tpl := Template{
+				LoaderX86: ".code32",
+			}
+
+			err := tpl.Check()
+			errStr := "empty loader template for x64"
+			require.EqualError(t, err, errStr)
+		})
+	})
+
+	t.Run("invalid num inst", func(t *testing.T) {
+		t.Run("x86", func(t *testing.T) {
+			tpl := Template{
+				LoaderX86:     ".code32",
+				LoaderX64:     ".code64",
+				MaxNumInstX86: 0,
+			}
+
+			err := tpl.Check()
+			errStr := "invalid maximum number of loader instructions for x86"
+			require.EqualError(t, err, errStr)
+		})
+
+		t.Run("x64", func(t *testing.T) {
+			tpl := Template{
+				LoaderX86:     ".code32",
+				LoaderX64:     ".code64",
+				MaxNumInstX86: 86,
+				MaxNumInstX64: 0,
+			}
+
+			err := tpl.Check()
+			errStr := "invalid maximum number of loader instructions for x64"
+			require.EqualError(t, err, errStr)
+		})
+	})
 }
