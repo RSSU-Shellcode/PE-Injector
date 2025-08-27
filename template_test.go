77d33592a78a37da1fd3e7c8911a1765bfa2ba77
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-08-27 12:53:52 +0800

    move the code about loader template to new file in testdata.

--- template_test.go
+++ template_test.go
@@ -9,101 +9,15 @@ import (
 )
 
 func TestTemplate(t *testing.T) {
-	loaderX86 := `
-.code32
+	injector := NewInjector()
 
-entry:
-  // check Integer
-  mov {{.Reg.ecx}}, {{hex .CIEnc.Const_1}}
-  xor {{.Reg.ecx}}, {{hex .CIKey.Const_1}}
-  cmp {{.Reg.ecx}}, 123
-  jne panic
-
-  mov {{.Reg.ecx}}, {{hex .CIEnc.Const_2}}
-  xor {{.Reg.ecx}}, {{hex .CIKey.Const_2}}
-  cmp {{.Reg.ecx}}, 456
-  jne panic
-
-  // check ANSI
-  mov {{.Reg.eax}}, {{index .CAEnc.ANSI_1 0}}
-  mov {{.Reg.ecx}}, {{index .CAKey.ANSI_1 0}}
-  xor {{.Reg.eax}}, {{.Reg.ecx}}
-  push {{.Reg.eax}}
-  mov {{.Reg.eax}}, {{index .CAEnc.ANSI_1 1}}
-  mov {{.Reg.ecx}}, {{index .CAKey.ANSI_1 1}}
-  xor {{.Reg.eax}}, {{.Reg.ecx}}
-  push {{.Reg.eax}}
-
-  // "ansi"
-  push 0x69736E61
-
-  // compare string
-  mov esi, esp
-  lea edi, [esp+4]
-  mov ecx, 4
-  cld
-  repe cmpsb
-  jnz panic
-
-  // check UTF-16
-  mov {{.Reg.eax}}, {{index .CWEnc.UTF16_1 0}}
-  mov {{.Reg.ecx}}, {{index .CWKey.UTF16_1 0}}
-  xor {{.Reg.eax}}, {{.Reg.ecx}}
-  push {{.Reg.eax}}
-  mov {{.Reg.eax}}, {{index .CWEnc.UTF16_1 1}}
-  mov {{.Reg.ecx}}, {{index .CWKey.UTF16_1 1}}
-  xor {{.Reg.eax}}, {{.Reg.ecx}}
-  push {{.Reg.eax}}
-  mov {{.Reg.eax}}, {{index .CWEnc.UTF16_1 2}}
-  mov {{.Reg.ecx}}, {{index .CWKey.UTF16_1 2}}
-  xor {{.Reg.eax}}, {{.Reg.ecx}}
-  push {{.Reg.eax}}
-
-  // "utf16"
-  push 0x00000036
-  push 0x00310066
-  push 0x00740075
-
-  // compare string
-  mov esi, esp
-  lea edi, [esp+3*4]
-  mov ecx, 10
-  cld
-  repe cmpsb
-  jnz panic
-
-  // check Arguments
-  mov {{.Reg.ecx}}, {{.Args.Arg_1}}
-  cmp {{.Reg.ecx}}, 123
-  jne panic
-  mov {{.Reg.ecx}}, {{.Args.Arg_2}}
-  cmp {{.Reg.ecx}}, 456
-  jne panic
-
-  // check Switches
-  {{if .Switches.Switch_1}}
-    jmp panic
-  {{end}}
-
-  {{if not .Switches.Switch_2}}
-    jmp panic
-  {{end}}
-
-  add esp, 9*4
-
-  // mark the end of loader
-  {{db .EndOfLoader}}
-
-panic:
-  int3
-`
-	loaderX64 := `
-.code64
-
-`
+	loaderX86, err := os.ReadFile("testdata/template_x86.asm")
+	require.NoError(t, err)
+	loaderX64, err := os.ReadFile("testdata/template_x64.asm")
+	require.NoError(t, err)
 	tpl := &Template{
-		LoaderX86:     loaderX86,
-		LoaderX64:     loaderX64,
+		LoaderX86:     string(loaderX86),
+		LoaderX64:     string(loaderX64),
 		MaxNumInstX86: 150,
 		MaxNumInstX64: 100,
 
@@ -130,9 +44,6 @@ panic:
 			"Switch_2": true,
 		},
 	}
-
-	injector := NewInjector()
-
 	opts := &Options{
 		Template: tpl,
 	}
@@ -165,7 +76,7 @@ panic:
 		testExecuteImage(t, "testdata/injected_x64.exe", ctx.Output)
 	})
 
-	err := injector.Close()
+	err = injector.Close()
 	require.NoError(t, err)
 }
 
