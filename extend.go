c4ae83729461966522ffc9634bde00b59ea0a502
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2025-12-28 16:51:48 +0800

    update extendTextSection about adjust other section headers.

--- extend.go
+++ extend.go
@@ -11,6 +11,7 @@ func (inj *Injector) extendTextSection(size uint32) ([]byte, error) {
 	if !inj.canTryExtend {
 		return nil, errors.New("the first section without RX")
 	}
+	size = alignFileOffset(size)
 	output := make([]byte, len(inj.dup)+int(size))
 	// copy all data before the first section data
 	copy(output, inj.dup[:inj.img.Sections[0].Offset])
@@ -19,24 +20,34 @@ func (inj *Injector) extendTextSection(size uint32) ([]byte, error) {
 	text := new(pe.SectionHeader32)
 	_ = binary.Read(bytes.NewReader(inj.dup[shOffset:]), binary.LittleEndian, text)
 	text.VirtualSize += size
-	text.SizeOfRawData += alignFileOffset(size)
+	text.SizeOfRawData += size
 	// overwrite the text section
 	buffer := bytes.NewBuffer(nil)
 	_ = binary.Write(buffer, binary.LittleEndian, text)
 	copy(output[shOffset:], buffer.Bytes())
+	// copy text section data
+	textPtr := text.PointerToRawData
+	copy(output[textPtr+size:], inj.dup[textPtr:])
 	// adjust other section headers
 	prev := text
 	for i := 1; i < len(inj.img.Sections); i++ {
 		shOffset += imageSectionHeaderSize
 		section := new(pe.SectionHeader32)
 		_ = binary.Read(bytes.NewReader(inj.dup[shOffset:]), binary.LittleEndian, section)
-		section.VirtualAddress = prev.VirtualAddress + alignFileOffset(prev.VirtualSize)
-		section.PointerToRawData += alignFileOffset(size)
+		section.VirtualAddress = prev.VirtualAddress + alignMemoryRegion(prev.VirtualSize)
+		section.PointerToRawData += size
 		// rewrite section header
 		buffer.Reset()
-		_ = binary.Write(buffer, binary.LittleEndian, text)
+		_ = binary.Write(buffer, binary.LittleEndian, section)
 		copy(output[shOffset:], buffer.Bytes())
+		// copy section data
+		setPtr := section.PointerToRawData
+		copy(output[setPtr:], inj.dup[setPtr-size:])
+		// update previous section
+		prev = section
 	}
+	// adjust data directory
+
 	// adjust the size of image in optional header
 	buffer.Reset()
 	switch inj.arch {
