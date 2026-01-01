82fa3af6c41f10bc1037ca1b3db96b447a001af4
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-01-01 15:39:12 +0800

    add TestExtendTextSection_DLL for check adjustExportDirectory.

--- extend_test.go
+++ extend_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestExtendTextSection(t *testing.T) {
+func TestExtendTextSection_EXE(t *testing.T) {
 	injector := NewInjector()
 
 	t.Run("x86", func(t *testing.T) {
@@ -32,6 +32,39 @@ func TestExtendTextSection(t *testing.T) {
 		require.NoError(t, err)
 
 		testExecuteImage(t, "testdata/extended_x64.exe", output)
+	})
+
+	err := injector.Close()
+	require.NoError(t, err)
+}
+
+func TestExtendTextSection_DLL(t *testing.T) {
+	injector := NewInjector()
+
+	t.Run("x86", func(t *testing.T) {
+		image, err := os.ReadFile("testdata/kernel32_x86.dat")
+		require.NoError(t, err)
+		err = injector.preprocess(image, nil)
+		require.NoError(t, err)
+
+		output, err := injector.extendTextSection(1427)
+		require.NoError(t, err)
+
+		err = os.WriteFile("testdata/extended_x86.dll", output, 0600)
+		require.NoError(t, err)
+	})
+
+	t.Run("x64", func(t *testing.T) {
+		image, err := os.ReadFile("testdata/kernel32_x64.dat")
+		require.NoError(t, err)
+		err = injector.preprocess(image, nil)
+		require.NoError(t, err)
+
+		output, err := injector.extendTextSection(7433)
+		require.NoError(t, err)
+
+		err = os.WriteFile("testdata/extended_x64.dll", output, 0600)
+		require.NoError(t, err)
 	})
 
 	err := injector.Close()
