fe376748838bdb753b7c343f3e1d0d6dd7118acf
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-01-03 14:22:44 +0800

    image/exe/build.*: adjust output path, not use command about move.

--- testdata/image/exe/build.bat
+++ testdata/image/exe/build.bat
@@ -1,6 +1,4 @@
 set GOARCH=386
-go build -v -trimpath -ldflags "-s -w" -o image_x86.dat
+go build -v -trimpath -ldflags "-s -w" -o ../../image_exe_x86.dat
 set GOARCH=amd64
-go build -v -trimpath -ldflags "-s -w" -o image_x64.dat
-move image_x86.dat ../
-move image_x64.dat ../
+go build -v -trimpath -ldflags "-s -w" -o ../../image_exe_x64.dat
--- testdata/image/exe/build.sh
+++ testdata/image/exe/build.sh
@@ -1,7 +1,5 @@
 export GOOS=windows
 export GOARCH=386
-go build -v -trimpath -ldflags "-s -w" -o image_x86.dat
+go build -v -trimpath -ldflags "-s -w" -o ../../image_exe_x86.dat
 export GOARCH=amd64
-go build -v -trimpath -ldflags "-s -w" -o image_x64.dat
-mv image_x86.dat ../
-mv image_x64.dat ../
+go build -v -trimpath -ldflags "-s -w" -o ../../image_exe_x64.dat
