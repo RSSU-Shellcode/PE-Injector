014c805323634e375d9f2da1cd617f5a6affa0d1
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-01-05 15:44:24 +0800

    image/dll/Clean.bat: add a clean script for clean output files.

---
+++ testdata/image/dll/Clean.bat
@@ -0,0 +1,9 @@
+rd  /S /Q ".vs\TestDLL\v17\ipch"
+del /S /Q ".vs\TestDLL\v17\Browse.VC.db"
+del /S /Q ".vs\TestDLL\v17\Solution.VC.db"
+
+rd /S /Q "TestDLL"
+rd /S /Q "Debug"
+rd /S /Q "Release"
+rd /S /Q "x64"
+rd /S /Q "x86"
