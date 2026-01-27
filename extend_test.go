39e91dcec5d950e19ebf96dd23347d305fd35f01
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-01-27 13:49:14 +0800

    merge TestExtendTextSection_EXE and DLL to one test, update file path.

--- extend_test.go
+++ extend_test.go
@@ -7,64 +7,61 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestExtendTextSection_EXE(t *testing.T) {
+func TestExtendTextSection(t *testing.T) {
 	injector := NewInjector()
 
-	t.Run("x86", func(t *testing.T) {
-		image, err := os.ReadFile("testdata/image_x86.dat")
-		require.NoError(t, err)
-		err = injector.preprocess(image, nil)
-		require.NoError(t, err)
+	t.Run("exe", func(t *testing.T) {
+		t.Run("x86", func(t *testing.T) {
+			image, err := os.ReadFile("testdata/image_exe_x86.dat")
+			require.NoError(t, err)
+			err = injector.preprocess(image, nil)
+			require.NoError(t, err)
 
-		output, err := injector.extendTextSection(1427)
-		require.NoError(t, err)
+			output, err := injector.extendTextSection(1427)
+			require.NoError(t, err)
 
-		testExecuteEXE(t, "testdata/extended_x86.exe", output)
+			testExecuteEXE(t, "testdata/extended_x86.exe", output)
+		})
+
+		t.Run("x64", func(t *testing.T) {
+			image, err := os.ReadFile("testdata/image_exe_x64.dat")
+			require.NoError(t, err)
+			err = injector.preprocess(image, nil)
+			require.NoError(t, err)
+
+			output, err := injector.extendTextSection(7433)
+			require.NoError(t, err)
+
+			testExecuteEXE(t, "testdata/extended_x64.exe", output)
+		})
 	})
 
-	t.Run("x64", func(t *testing.T) {
-		image, err := os.ReadFile("testdata/image_x64.dat")
-		require.NoError(t, err)
-		err = injector.preprocess(image, nil)
-		require.NoError(t, err)
+	t.Run("dll", func(t *testing.T) {
+		t.Run("x86", func(t *testing.T) {
+			image, err := os.ReadFile("testdata/kernel32_x86.dat")
+			require.NoError(t, err)
+			err = injector.preprocess(image, nil)
+			require.NoError(t, err)
 
-		output, err := injector.extendTextSection(7433)
-		require.NoError(t, err)
+			output, err := injector.extendTextSection(1427)
+			require.NoError(t, err)
 
-		testExecuteEXE(t, "testdata/extended_x64.exe", output)
-	})
+			err = os.WriteFile("testdata/extended_x86.dll", output, 0600)
+			require.NoError(t, err)
+		})
 
-	err := injector.Close()
-	require.NoError(t, err)
-}
+		t.Run("x64", func(t *testing.T) {
+			image, err := os.ReadFile("testdata/kernel32_x64.dat")
+			require.NoError(t, err)
+			err = injector.preprocess(image, nil)
+			require.NoError(t, err)
 
-func TestExtendTextSection_DLL(t *testing.T) {
-	injector := NewInjector()
+			output, err := injector.extendTextSection(7433)
+			require.NoError(t, err)
 
-	t.Run("x86", func(t *testing.T) {
-		image, err := os.ReadFile("testdata/kernel32_x86.dat")
-		require.NoError(t, err)
-		err = injector.preprocess(image, nil)
-		require.NoError(t, err)
-
-		output, err := injector.extendTextSection(1427)
-		require.NoError(t, err)
-
-		err = os.WriteFile("testdata/extended_x86.dll", output, 0600)
-		require.NoError(t, err)
-	})
-
-	t.Run("x64", func(t *testing.T) {
-		image, err := os.ReadFile("testdata/kernel32_x64.dat")
-		require.NoError(t, err)
-		err = injector.preprocess(image, nil)
-		require.NoError(t, err)
-
-		output, err := injector.extendTextSection(7433)
-		require.NoError(t, err)
-
-		err = os.WriteFile("testdata/extended_x64.dll", output, 0600)
-		require.NoError(t, err)
+			err = os.WriteFile("testdata/extended_x64.dll", output, 0600)
+			require.NoError(t, err)
+		})
 	})
 
 	err := injector.Close()
