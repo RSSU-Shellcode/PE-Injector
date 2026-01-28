6493a3fd5353d39e8ec9f2f9a208858d35e2ee39
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-01-28 13:03:52 +0800

    update TestExtendTextSection about test extended dll file.

--- extend_test.go
+++ extend_test.go
@@ -2,6 +2,7 @@ package injector
 
 import (
 	"os"
+	"runtime"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -38,7 +39,7 @@ func TestExtendTextSection(t *testing.T) {
 
 	t.Run("dll", func(t *testing.T) {
 		t.Run("x86", func(t *testing.T) {
-			image, err := os.ReadFile("testdata/kernel32_x86.dat")
+			image, err := os.ReadFile("testdata/image_dll_x86.dat")
 			require.NoError(t, err)
 			err = injector.preprocess(image, nil)
 			require.NoError(t, err)
@@ -46,12 +47,14 @@ func TestExtendTextSection(t *testing.T) {
 			output, err := injector.extendTextSection(1427)
 			require.NoError(t, err)
 
-			err = os.WriteFile("testdata/extended_x86.dll", output, 0600)
-			require.NoError(t, err)
+			if runtime.GOARCH != "386" {
+				return
+			}
+			testExecuteDLL(t, "testdata/extended_x86.dll", output)
 		})
 
 		t.Run("x64", func(t *testing.T) {
-			image, err := os.ReadFile("testdata/kernel32_x64.dat")
+			image, err := os.ReadFile("testdata/image_dll_x64.dat")
 			require.NoError(t, err)
 			err = injector.preprocess(image, nil)
 			require.NoError(t, err)
@@ -59,8 +62,10 @@ func TestExtendTextSection(t *testing.T) {
 			output, err := injector.extendTextSection(7433)
 			require.NoError(t, err)
 
-			err = os.WriteFile("testdata/extended_x64.dll", output, 0600)
-			require.NoError(t, err)
+			if runtime.GOARCH != "amd64" {
+				return
+			}
+			testExecuteDLL(t, "testdata/extended_x64.dll", output)
 		})
 	})
 
