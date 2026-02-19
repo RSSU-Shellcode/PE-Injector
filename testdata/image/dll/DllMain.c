278e0f63b57c11ef5707bd05598914e8396714b4
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-02-19 14:24:55 +0800

    image/dll/DllMain.c: add Load for let procedure LoadLibraryW and GetProcAddress in IAT.

--- testdata/image/dll/DllMain.c
+++ testdata/image/dll/DllMain.c
@@ -1,5 +1,10 @@
 ﻿#include <windows.h>
 
+typedef void (*Sleep_t)
+(
+    DWORD dwMilliseconds
+);
+
 BOOL DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved)
 {
     return TRUE;
@@ -8,4 +13,21 @@ BOOL DllMain(HMODULE hModule, DWORD reason, LPVOID lpReserved)
 INT32 Add(INT32 a, INT32 b)
 {
     return a + b;
+}
+
+// only let procedure LoadLibraryW and GetProcAddress in IAT
+BOOL Load()
+{
+    HMODULE hKernel32 = LoadLibrary(L"kernel32.dll");
+    if (hKernel32 == NULL)
+    {
+        return FALSE;
+    }
+    Sleep_t Sleep = (Sleep_t)GetProcAddress(hKernel32, "Sleep");
+    if (Sleep == NULL)
+    {
+        return FALSE;
+    }
+    Sleep(100);
+    return TRUE;
 }
