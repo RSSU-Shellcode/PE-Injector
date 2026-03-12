c49c87ceaa4799fa723a475a0e61ac1420dda8eb
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-03-12 13:01:00 +0800

    add TestBuildRandomRegisterMap for check the new shuffle algorithm.

--- assembly_test.go
+++ assembly_test.go
@@ -8,6 +8,37 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestBuildRandomRegisterMap(t *testing.T) {
+	injector := NewInjector()
+
+	t.Run("x86", func(t *testing.T) {
+		injector.arch = "386"
+		err := injector.initAssembler()
+		require.NoError(t, err)
+
+		regMap := injector.buildRandomRegisterMap()
+		for src, dst := range regMap {
+			fmt.Printf("%s -> %s\n", src, dst)
+			require.NotEqual(t, src, dst)
+		}
+	})
+
+	t.Run("x64", func(t *testing.T) {
+		injector.arch = "amd64"
+		err := injector.initAssembler()
+		require.NoError(t, err)
+
+		regMap := injector.buildRandomRegisterMap()
+		for src, dst := range regMap {
+			fmt.Printf("%s -> %s\n", src, dst)
+			require.NotEqual(t, src, dst)
+		}
+	})
+
+	err := injector.Close()
+	require.NoError(t, err)
+}
+
 func TestRelocateInstructionSegment(t *testing.T) {
 	injector := NewInjector()
 
