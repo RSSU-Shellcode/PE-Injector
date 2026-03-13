94e6f9b96f98add92c236da9756afa4c0898c2cf
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-03-13 15:01:43 +0800

    add sub tests about invalid arch and invalid template, update all tests.

--- inspect_test.go
+++ inspect_test.go
@@ -21,6 +21,13 @@ func TestInspectLoaderTemplate(t *testing.T) {
 		require.Nil(t, cfg)
 	})
 
+	t.Run("invalid arch", func(t *testing.T) {
+		cfg, err := InspectLoaderTemplate("invalid", "")
+		require.EqualError(t, err, "unsupported architecture: invalid")
+		require.NotNil(t, cfg)
+		spew.Dump(cfg)
+	})
+
 	t.Run("invalid template", func(t *testing.T) {
 		cfg, err := InspectLoaderTemplate("amd64", "invalid")
 		errStr := "failed to assemble loader: failed to assemble: "
@@ -37,31 +44,41 @@ func TestInspectLoaderTemplateWithConfig(t *testing.T) {
 	t.Run("x86", func(t *testing.T) {
 		for _, cfg := range configs {
 			asm, inst, err := InspectLoaderTemplateWithConfig("386", defaultLoaderX86, cfg)
-			require.NoError(t, err, asm)
+			require.NoError(t, err)
 			insts, err := disassemble(inst, 32)
 			require.NoError(t, err, inst)
 
 			fmt.Println("num of instructions:", len(insts))
 			spew.Dump(inst)
+			fmt.Println(asm)
 		}
 	})
 
 	t.Run("x64", func(t *testing.T) {
 		for _, cfg := range configs {
 			asm, inst, err := InspectLoaderTemplateWithConfig("amd64", defaultLoaderX64, cfg)
-			require.NoError(t, err, asm)
+			require.NoError(t, err)
 			insts, err := disassemble(inst, 64)
 			require.NoError(t, err, inst)
 
 			fmt.Println("num of instructions:", len(insts))
 			spew.Dump(inst)
+			fmt.Println(asm)
 		}
 	})
 
 	t.Run("invalid arch", func(t *testing.T) {
 		cfg := InspectConfig{}
-		asm, inst, err := InspectLoaderTemplateWithConfig("test", "", &cfg)
-		require.EqualError(t, err, "unsupported architecture: test")
+		asm, inst, err := InspectLoaderTemplateWithConfig("invalid", "", &cfg)
+		require.EqualError(t, err, "unsupported architecture: invalid")
+		require.Nil(t, inst)
+		require.Zero(t, asm)
+	})
+
+	t.Run("invalid template", func(t *testing.T) {
+		cfg := InspectConfig{}
+		asm, inst, err := InspectLoaderTemplateWithConfig("amd64", "invalid", &cfg)
+		require.EqualError(t, err, "proc LoadLibrary is not exist in IAT")
 		require.Nil(t, inst)
 		require.Zero(t, asm)
 	})
@@ -71,30 +88,41 @@ func TestInspectJunkCodeTemplate(t *testing.T) {
 	t.Run("x86", func(t *testing.T) {
 		for _, src := range defaultJunkCodeX86 {
 			asm, inst, err := InspectJunkCodeTemplate("386", src)
-			require.NoError(t, err, asm)
+			require.NoError(t, err)
 			insts, err := disassemble(inst, 32)
 			require.NoError(t, err, inst)
 
 			fmt.Println("num of instructions:", len(insts))
 			spew.Dump(inst)
+			fmt.Println(asm)
 		}
 	})
 
 	t.Run("x64", func(t *testing.T) {
 		for _, src := range defaultJunkCodeX64 {
 			asm, inst, err := InspectJunkCodeTemplate("amd64", src)
-			require.NoError(t, err, asm)
+			require.NoError(t, err)
 			insts, err := disassemble(inst, 64)
 			require.NoError(t, err, inst)
 
 			fmt.Println("num of instructions:", len(insts))
 			spew.Dump(inst)
+			fmt.Println(asm)
 		}
 	})
 
 	t.Run("invalid arch", func(t *testing.T) {
-		asm, inst, err := InspectJunkCodeTemplate("test", "")
-		require.EqualError(t, err, "unsupported architecture: test")
+		asm, inst, err := InspectJunkCodeTemplate("invalid", "")
+		require.EqualError(t, err, "unsupported architecture: invalid")
+		require.Nil(t, inst)
+		require.Zero(t, asm)
+	})
+
+	t.Run("invalid template", func(t *testing.T) {
+		asm, inst, err := InspectJunkCodeTemplate("amd64", "invalid")
+		errStr := "failed to assemble junk code: failed to assemble: "
+		errStr += "Invalid mnemonic (KS_ERR_ASM_MNEMONICFAIL)"
+		require.EqualError(t, err, errStr)
 		require.Nil(t, inst)
 		require.Zero(t, asm)
 	})
