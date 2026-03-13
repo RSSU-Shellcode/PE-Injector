394485a59155cc4e6d9a6aa1ad69b8b35c1eddb4
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-03-13 12:48:22 +0800

    add TestInspectLoaderTemplateWithConfig, rewrite TestInspectLoaderTemplate.

--- inspect_test.go
+++ inspect_test.go
@@ -9,50 +9,22 @@ import (
 )
 
 func TestInspectLoaderTemplate(t *testing.T) {
-	var configs []*InspectConfig
-	config := InspectConfig{}
-	for i := 0; i < 5; i++ {
-		config.CodeCaveMode = i == 0
-		config.CodeCaveNSMode = i == 1
-		config.ExtendTextMode = i == 2
-		config.ExtendTextNSMode = i == 3
-		config.CreateTextMode = i == 4
+	t.Run("x86", func(t *testing.T) {
+		err := InspectLoaderTemplate("x86", defaultLoaderX86)
+		require.NoError(t, err)
+	})
 
-		for i1 := 0; i1 < 2; i1++ {
-			config.HasVirtualAlloc = i1 == 0
-			for i2 := 0; i2 < 2; i2++ {
-				config.HasVirtualFree = i2 == 0
-				for i3 := 0; i3 < 2; i3++ {
-					config.HasVirtualProtect = i3 == 0
-					for i4 := 0; i4 < 2; i4++ {
-						config.HasCreateThread = i4 == 0
-						for i5 := 0; i5 < 2; i5++ {
-							config.HasWaitForSingleObject = i5 == 0
+	t.Run("x64", func(t *testing.T) {
+		err := InspectLoaderTemplate("x64", defaultLoaderX64)
+		require.NoError(t, err)
+	})
+}
 
-							config.HasLoadLibraryA = true
-							config.HasLoadLibraryW = false
-							cp := config
-							configs = append(configs, &cp)
-
-							config.HasLoadLibraryA = false
-							config.HasLoadLibraryW = true
-							cp = config
-							configs = append(configs, &cp)
-
-							config.HasLoadLibraryA = true
-							config.HasLoadLibraryW = true
-							cp = config
-							configs = append(configs, &cp)
-						}
-					}
-				}
-			}
-		}
-	}
-
+func TestInspectLoaderTemplateWithConfig(t *testing.T) {
+	configs := buildPossibleConfigs()
 	t.Run("x86", func(t *testing.T) {
 		for _, cfg := range configs {
-			asm, inst, err := InspectLoaderTemplate("386", defaultLoaderX86, cfg)
+			asm, inst, err := InspectLoaderTemplateWithConfig("386", defaultLoaderX86, cfg)
 			require.NoError(t, err, asm)
 			insts, err := disassemble(inst, 32)
 			require.NoError(t, err, inst)
@@ -64,7 +36,7 @@ func TestInspectLoaderTemplate(t *testing.T) {
 
 	t.Run("x64", func(t *testing.T) {
 		for _, cfg := range configs {
-			asm, inst, err := InspectLoaderTemplate("amd64", defaultLoaderX64, cfg)
+			asm, inst, err := InspectLoaderTemplateWithConfig("amd64", defaultLoaderX64, cfg)
 			require.NoError(t, err, asm)
 			insts, err := disassemble(inst, 64)
 			require.NoError(t, err, inst)
@@ -76,7 +48,7 @@ func TestInspectLoaderTemplate(t *testing.T) {
 
 	t.Run("invalid arch", func(t *testing.T) {
 		cfg := InspectConfig{}
-		asm, inst, err := InspectLoaderTemplate("test", "", &cfg)
+		asm, inst, err := InspectLoaderTemplateWithConfig("test", "", &cfg)
 		require.EqualError(t, err, "unsupported architecture: test")
 		require.Nil(t, inst)
 		require.Zero(t, asm)
