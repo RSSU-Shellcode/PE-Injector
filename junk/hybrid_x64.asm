8aa8dc56b108e545542e49638df28bba6d1004d6
For-ACGN <38399410+For-ACGN@users.noreply.github.com>
2026-03-16 13:48:40 +0800

    hybrid_*.asm: improve template that prevent appear jmp 0 and jnz 0.

--- junk/hybrid_x64.asm
+++ junk/hybrid_x64.asm
@@ -4,44 +4,52 @@ entry:
   pushfq
   push {{.Reg.rax}}
 
-  {{if .Switch.A}}
+{{if .Switch.A}}
   mov {{.Reg.rax}}, {{.Reg.rbx}}
   test {{.Reg.rax}}, {{.Reg.rax}}
   jnz next_1
+  xor {{.Reg.rax}}, {{.Reg.rsi}}
   {{iji}}
  next_1:
   call func_1
-  {{end}}
+{{end}}
 
-  {{if .Switch.B}}
+{{if .Switch.B}}
   xor {{.Reg.rax}}, {{.Reg.rcx}}
   {{iji}}
   jmp leave
-  {{end}}
+{{end}}
 
-  {{if .Switch.C}}
+  xor {{.Reg.rax}}, {{.Reg.rdi}}
+
+{{if .Switch.C}}
   xor {{.Reg.rax}}, {{.DWORD.D}}
   call func_2
-  {{end}}
+{{end}}
 
-  {{if .Switch.D}}
+{{if .Switch.D}}
   mov {{.Reg.rax}}, {{.WORD.E}}
   test {{.Reg.rax}}, {{.Reg.rax}}
   jnz next_3
+  call func_1
   {{iji}}
  next_3:
-  {{iji}}
-  jmp leave
-  {{end}}
-
   {{if .Switch.E}}
+    {{iji}}
+  {{end}}
+  jmp leave
+{{end}}
+
+  call func_2
+
+{{if .Switch.F}}
   ror {{.Reg.rax}}, {{.Less32.A}}
   xor {{.Reg.rax}}, {{.Reg.rdi}}
   rol {{.Reg.rax}}, {{.Less32.B}}
   call func_3
-  {{end}}
+{{end}}
 
-leave:
+ leave:
   pop {{.Reg.rax}}
   popfq
   jmp exit
@@ -53,21 +61,22 @@ func_1:
 func_2:
   push {{.Reg.rbx}}
   mov {{.Reg.rbx}}, {{.Reg.rcx}}
-  {{if .Switch.F}}
-    test {{.Reg.rbx}}, {{.Reg.rbx}}
-    jnz next_2
-    {{iji}}
-   next_2:
-    call func_1
-  {{end}}
+{{if .Switch.G}}
+  test {{.Reg.rbx}}, {{.Reg.rbx}}
+  jnz next_2
+  call func_3
+  {{iji}}
+ next_2:
+  call func_1
+{{end}}
   pop {{.Reg.rbx}}
   ret
 
 func_3:
   rol {{.Reg.rax}}, {{.Less32.C}}
-  {{if .Switch.E}}
+{{if .Switch.H}}
   {{iji}}
-  {{end}}
+{{end}}
   ret
 
 exit:
--- junk/hybrid_x86.asm
+++ junk/hybrid_x86.asm
@@ -1,67 +1,54 @@
 .code32
 
-pushfd
-push {{.Reg.eax}}
 entry:
   pushfd
   push {{.Reg.eax}}
 
 {{if .Switch.A}}
-mov {{.Reg.eax}}, {{.Reg.ebx}}
-{{end}}
-  {{if .Switch.A}}
   mov {{.Reg.eax}}, {{.Reg.ebx}}
   test {{.Reg.eax}}, {{.Reg.eax}}
   jnz next_1
+  xor {{.Reg.eax}}, {{.Reg.esi}}
   {{iji}}
  next_1:
   call func_1
-  {{end}}
+{{end}}
 
 {{if .Switch.B}}
-xor {{.Reg.eax}}, {{.Reg.ecx}}
-{{end}}
-  {{if .Switch.B}}
   xor {{.Reg.eax}}, {{.Reg.ecx}}
   {{iji}}
   jmp leave
-  {{end}}
+{{end}}
+
+  xor {{.Reg.eax}}, {{.Reg.edi}}
 
 {{if .Switch.C}}
-xor {{.Reg.eax}}, {{.DWORD.D}}
-{{end}}
-  {{if .Switch.C}}
   xor {{.Reg.eax}}, {{.DWORD.D}}
   call func_2
-  {{end}}
+{{end}}
 
 {{if .Switch.D}}
-mov {{.Reg.eax}}, {{.WORD.E}}
-{{end}}
-  {{if .Switch.D}}
   mov {{.Reg.eax}}, {{.WORD.E}}
   test {{.Reg.eax}}, {{.Reg.eax}}
   jnz next_3
+  call func_1
   {{iji}}
  next_3:
-  {{iji}}
-  jmp leave
-  {{end}}
-
-{{if .Switch.E}}
-ror {{.Reg.eax}}, {{.Less32.A}}
-xor {{.Reg.eax}}, {{.Reg.edi}}
-rol {{.Reg.eax}}, {{.Less32.B}}
-{{end}}
   {{if .Switch.E}}
+    {{iji}}
+  {{end}}
+  jmp leave
+{{end}}
+
+  call func_2
+
+{{if .Switch.F}}
   ror {{.Reg.eax}}, {{.Less32.A}}
   xor {{.Reg.eax}}, {{.Reg.edi}}
   rol {{.Reg.eax}}, {{.Less32.B}}
   call func_3
-  {{end}}
+{{end}}
 
-pop {{.Reg.eax}}
-popfd
  leave:
   pop {{.Reg.eax}}
   popfd
@@ -74,21 +61,22 @@ func_1:
 func_2:
   push {{.Reg.ebx}}
   mov {{.Reg.ebx}}, {{.Reg.ecx}}
-  {{if .Switch.F}}
-    test {{.Reg.ebx}}, {{.Reg.ebx}}
-    jnz next_2
-    {{iji}}
-   next_2:
-    call func_1
-  {{end}}
+{{if .Switch.G}}
+  test {{.Reg.ebx}}, {{.Reg.ebx}}
+  jnz next_2
+  call func_3
+  {{iji}}
+ next_2:
+  call func_1
+{{end}}
   pop {{.Reg.ebx}}
   ret
 
 func_3:
   rol {{.Reg.eax}}, {{.Less32.C}}
-  {{if .Switch.E}}
+{{if .Switch.H}}
   {{iji}}
-  {{end}}
+{{end}}
   ret
 
 exit:
